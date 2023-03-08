eab3f8df31752ebe5acca2394c9072af8f7e1272
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-08 14:33:46 -0500

    push
    Former-commit-id: 296a6719ca441a1886a191101a9f7b5cf8feda34

---
+++ helper-functions/coingecko_utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import requests as r
+
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
+
+def get_daily_token_data(cg_id, show_current=True):
+        info_api = 'https://api.coingecko.com/api/v3/coins/' + cg_id
+        info = r.get(info_api, headers = header).json()
+        symbol = info['symbol']
+        cg_name = info['name']
+        details = info['detail_platforms']
+
+        api_base = 'https://api.coingecko.com/api/v3/coins/' \
+                + cg_id \
+                + '/market_chart?vs_currency=usd&days=max&interval=daily'
+
+        res = r.get(api_base, headers = header).json()
+        prices = pd.DataFrame(res['prices'], columns = ['date','prices'])
+        mktcaps = pd.DataFrame(res['market_caps'], columns = ['date','market_caps'])
+        volumes = pd.DataFrame(res['total_volumes'], columns = ['date','total_volumes'])
+        arr_list = [prices, mktcaps, volumes]
+        final_df = pd.DataFrame()
+        for arr in arr_list:
+            arr['date'] = pd.to_datetime(arr['date'],unit='ms')
+            if final_df.empty:
+                final_df = arr
+            else:
+                final_df = final_df.merge(arr, on = 'date', how = 'outer')
+        
+        if show_current == False:
+            final_df.drop(final_df.tail(1).index,inplace=True) # drop last 1 row
+
+        final_df['cg_id'] = cg_id
+        final_df['symbol'] = symbol
+        final_df['name'] = cg_name
+        final_df['details'] = str(details)
+
+        return final_df
--- helper-functions/readme.md
+++ helper-functions/readme.md
@@ -1,6 +1,8 @@
 # Helper Functions
 This folder stores helper & utility functions to help with Optimism analytics work.
 
+*Note: Many of these functions are WIP & slightly messy. Docs will be updated as they become finalized.*
+
 APIs these functions work with include (but are not limited to):
 - [DefiLlama](https://defillama.com/docs/api)
 - [CoinGecko](https://www.coingecko.com/en/api)
