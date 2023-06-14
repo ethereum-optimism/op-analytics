974e372413e6db3f95b690dd46cf1f6b86f8887e
chuxinh <chuxin@optimism.io>
2023-06-14 20:09:21 +1000

    fix duneapi function
    Former-commit-id: ae5d6c8729bcf484efbfa76df3a4c770c1e18ad9

--- helper_functions/duneapi_utils.py
+++ helper_functions/duneapi_utils.py
@@ -9,11 +9,13 @@ import requests as r
 import re
 import json
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import sys
+
 sys.path.append("../helper_functions")
 import pandas_utils as pu
+
 sys.path.pop()
 
 from dune_client.types import QueryParameter
@@ -22,7 +24,11 @@ from dune_client.query import Query
 
 
 def get_dune_data(
-    query_id: int, name: str = "my_query_results", path: str = "csv_outputs", num_hours_to_rerun = 4, performance: str = "medium"
+    query_id: int,
+    name: str = "my_query_results",
+    path: str = "csv_outputs",
+    num_hours_to_rerun=4,
+    performance: str = "medium",
 ) -> pd.DataFrame:
     """
     Get data via Dune API.
@@ -36,16 +42,20 @@ def get_dune_data(
 
     dotenv.load_dotenv()
     dune = DuneClient(os.environ["DUNE_API_KEY"])
-    # get latest 
+    # get latest
     try:
         results = dune.get_latest_result(query_id)
         submitted_at = results.times.submitted_at
         # execution_started_at = latest.times.execution_started_at
         # execution_ended_at = latest.times.execution_ended_at
+
+        # Get the current time in the same timezone as submitted_at
+        current_time = datetime.now(submitted_at.tzinfo)
     except:
         submitted_at = 0
-    # Get the current time in the same timezone as submitted_at
-    current_time = datetime.now(submitted_at.tzinfo)
+        # Get the current time in UTC
+        current_time = datetime.now(timezone.utc)
+
     # print(current_time)
     # Calculate the time difference between the current time and submitted_at
     time_difference = current_time - submitted_at
@@ -56,13 +66,13 @@ def get_dune_data(
     else:
         results = dune.refresh(query)
         df = pd.DataFrame(results.result.rows)
-    
+
     df["last_updated"] = results.times.submitted_at
 
     if not os.path.exists(path):
         os.makedirs(path)
     # display(df)
-    df.to_csv(f"{path}/{name}.csv", escapechar='\\', index=False)
+    df.to_csv(f"{path}/{name}.csv", escapechar="\\", index=False)
 
     logger.info(
         f"✨ Results saved as {path}/{name}.csv, with {len(df)} rows and {len(df.columns)} columns."
@@ -70,7 +80,8 @@ def get_dune_data(
 
     return df
 
-def get_dune_data_raw(query_id, perf_var = "medium"):
+
+def get_dune_data_raw(query_id, perf_var="medium"):
     dotenv.load_dotenv()
     # authentiction with api key
     api_key = os.environ["DUNE_API_KEY"]
@@ -80,48 +91,52 @@ def get_dune_data_raw(query_id, perf_var = "medium"):
         "performance": perf_var,
     }
 
-
-    api_url = f"https://api.dune.com/api/v1/query/{query_id}/results?api_key={dune_api_key}"
-    df = pd.DataFrame(r.get(api_url).json()['rows'])
-    df = df.iloc[df.index.get_loc('rows')]
-    updated_at = df.iloc[0]['execution_ended_at']
-    df = df[['result']]
+    api_url = (
+        f"https://api.dune.com/api/v1/query/{query_id}/results?api_key={dune_api_key}"
+    )
+    df = pd.DataFrame(r.get(api_url).json()["rows"])
+    df = df.iloc[df.index.get_loc("rows")]
+    updated_at = df.iloc[0]["execution_ended_at"]
+    df = df[["result"]]
     return df
 
 
 def write_dune_api_from_csv(data, table_name, table_description):
-    headers = {'X-Dune-Api-Key': os.environ["DUNE_API_KEY"]}
+    headers = {"X-Dune-Api-Key": os.environ["DUNE_API_KEY"]}
     # Define the HTTP endpoint URL and data to send
-    url = 'https://api.dune.com/api/v1/table/upload/csv'
+    url = "https://api.dune.com/api/v1/table/upload/csv"
 
     payload = {
         "table_name": table_name,
         "description": table_description,
-        "data": str(data)
+        "data": str(data),
     }
-    
+
     response = r.post(url, data=json.dumps(payload), headers=headers)
 
-    print('Response status code:', response.status_code)
-    print('Response content:', response.content)
+    print("Response status code:", response.status_code)
+    print("Response content:", response.content)
+
 
 def write_dune_api_from_pandas(df, table_name, table_description):
-    #clean column names (replace spaces with _, force lowercase, remove quotes and other characters)
+    # clean column names (replace spaces with _, force lowercase, remove quotes and other characters)
     df = pu.formatted_columns_to_csv_format(df)
 
-    #iterate over columns and convert any columns containing hex values to strings
+    # iterate over columns and convert any columns containing hex values to strings
     for col in df.columns:
-        if df[col].dtype == 'O' and df[col].apply(lambda x: isinstance(x, str) and x.startswith('0x')).any():
+        if (
+            df[col].dtype == "O"
+            and df[col].apply(lambda x: isinstance(x, str) and x.startswith("0x")).any()
+        ):
             df[col] = df[col].astype(str).str.lower()
-            df[col] = df[col].replace('nan', np.nan)
+            df[col] = df[col].replace("nan", np.nan)
 
-    #convert pandas dataframe to csv
+    # convert pandas dataframe to csv
     data = df.to_csv(index=False)
     # print(data)
-    print("table at: dune_upload."+table_name)
+    print("table at: dune_upload." + table_name)
     # Write to Dune
     write_dune_api_from_csv(data, table_name, table_description)
-    
 
 
 if __name__ == "__main__":
