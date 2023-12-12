fa6b9c9cd14d35ffcd200b826b51023e1f7f64c5
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-12-12 14:27:37 -0500

    add
    Former-commit-id: 80062e5240f90ce8baa911e3564df7b755473f94

---
+++ helper_functions/opstack_metadata_utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+def generate_alignment_column(df):
+        conditions = [
+        df['is_op_chain'] == 'yes',
+        (df['is_op_chain'] == 'no'),
+        df['is_op_chain'].isna()
+        ]
+
+        choices = [
+        'OP Chain',
+        'OP Stack Fork',
+        'Other EVMs'
+        ]
+
+
+        df['alignment'] = np.select(conditions, choices, default='Unknown')
+        # Add 'Legacy ' prefix to 'alignment' if 'op_based_version' contains 'Legacy'
+        df['alignment'] = df.apply(lambda row: 'Legacy ' + row['alignment'] 
+                                if pd.notna(row['op_based_version']) and 'Legacy' in row['op_based_version'] 
+                                else row['alignment'], axis=1)
+
+        return df
