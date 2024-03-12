d00c0172e0b44c1564c1d840a926cf5d9c5bdf91
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-03-12 13:51:37 -0400

    push new l2beat
    Former-commit-id: 36560db4ac0bce371ef5d22cfafcb83a17c57684

--- helper_functions/l2beat_utils.py
+++ helper_functions/l2beat_utils.py
@@ -7,8 +7,10 @@ api_string = 'https://api.l2beat.com/api/'
 # https://l2beat.com/api/tvl/scaling.json
 # https://l2beat.com/api/tvl/optimism.json
 
-def get_l2beat_activity_data():
-        api_url = api_string + 'activity'
+def get_l2beat_activity_data(data='activity',granularity='daily'):
+        df = pd.DataFrame()
+
+        api_url = api_string + data
         response = r.get(api_url)
         response.raise_for_status()  # Check if the request was successful
         json_data = response.json()['projects']
@@ -19,8 +21,15 @@ def get_l2beat_activity_data():
         # Iterate over the chains
         for chain_name in json_data:
         # if chain_name != 'combined':
-                daily_data = json_data[chain_name]['daily']['data']
-                types = json_data[chain_name]['daily']['types']
+                if data == 'activity':
+                        daily_data = json_data[chain_name][granularity]['data']
+                        types = json_data[chain_name][granularity]['types']
+                elif data == 'tvl':
+                        daily_data = json_data[chain_name]['charts'][granularity]['data']
+                        types = json_data[chain_name]['charts'][granularity]['types']
+                else:
+                        print('not configured - need to configure for this API endpoint')
+                        return
 
                 # Iterate through each day's data
                 for day_data in daily_data:
@@ -33,3 +42,11 @@ def get_l2beat_activity_data():
         df = pd.DataFrame(rows_list)
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s') 
         return df
+
+def get_all_l2beat_data(granularity='daily'):
+        activity_df = get_l2beat_activity_data('activity',granularity)
+        tvl_df = get_l2beat_activity_data('tvl',granularity)
+
+        combined_df = tvl_df.merge(activity_df, on=['timestamp','chain'],how='outer')
+
+        return combined_df
