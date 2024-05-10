2448e1a32a8db6842319dd9aab5127278c64bd23
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-05-10 15:30:12 -0400

    pushhhhh

--- helper_functions/l2beat_utils.py
+++ helper_functions/l2beat_utils.py
@@ -74,6 +74,11 @@ def get_l2beat_metadata():
                 'slug': r"slug: '([^']+)'",
                 'imports': r"import {([^}]+)} from",
                 'provider': r"display:.*?provider: '([^']+)'",  # Updated to handle multiline and nested content
+                'hostChain': r"hostChain: ProjectId\('(\w+)'\)",
+                'websites': r"websites: \[([^\]]+)\]",
+                'documentation': r"documentation: \[([^\]]+)\]",
+                'repositories': r"repositories: \[([^\]]+)\]",
+                'rpcUrl': r"rpcUrl: '([^']+)'",
         }
         
         # Function to extract data using regular expressions
@@ -98,15 +103,15 @@ def get_l2beat_metadata():
                         items = [item.strip() for item in items if item.strip()]
                         imports.update(items)
                 return list(imports)
-        # Function to check if any config item contains the word 'upcoming'
+        # Function to check if any config item contains the word 'UpcomingL'
         def check_upcoming(configs):
-                return any('upcoming' in config.lower() for config in configs)
+                return any('upcomingl' in config.lower() for config in configs)
         def determine_provider(file_content):
-                        if 'opStackL2' in file_content:
+                        if 'opStackL' in file_content: #opStackL2 or opStackL3
                                 return 'OP Stack'
                         elif 'polygonCDKStack' in file_content:
                                 return 'Polygon CDK'
-                        elif 'orbitStackL3' in file_content:
+                        elif 'orbitStackL' in file_content: #orbitStackL2 or orbitStackL3
                                 return 'Arbitrum Orbit'
                         elif "'zkSync Lite'" in file_content:
                                 return 'ZKSync Lite'
@@ -157,8 +162,13 @@ def get_l2beat_metadata():
                                         'category': extract_data(file_content, patterns['category']) if layer_name in ['L2', 'L3'] else None,
                                         'slug': extract_data(file_content, patterns['slug']) or file['name'].replace('.ts', ''),  # Filename as fallback slug
                                         'provider': determine_provider(file_content),  # Determine provider with custom logic
+                                        'hostChain': extract_data(file_content, patterns['hostChain']),
+                                        'is_upcoming': is_upcoming,
+                                        'websites': extract_data(file_content, patterns['websites']),
+                                        'documentation': extract_data(file_content, patterns['documentation']),
+                                        'repositories': extract_data(file_content, patterns['repositories']),
+                                        'rpcUrl': extract_data(file_content, patterns['rpcUrl'])
                                         # 'configs': configs,  # Extract imports as a list
-                                        'is_upcoming': is_upcoming
                                 }
                                 
                                 # Add the data dictionary to our list
