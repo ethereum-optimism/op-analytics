a923459bcb02e52d448cae962350f51c3e6dd13c
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-06-03 09:42:08 -0400

    push updates

--- helper_functions/google_bq_utils.py
+++ helper_functions/google_bq_utils.py
@@ -3,13 +3,13 @@ import dotenv
 import json
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import pandas_utils as pu
 
 dotenv.load_dotenv()
 
-def connect_bq_client():
+def connect_bq_client(project_id = os.getenv("BQ_PROJECT_ID")):
         # Check if running locally
         is_running_local = os.environ.get("IS_RUNNING_LOCAL", "False").lower() == "true"
-        print(is_running_local)
 
         # Set the environment variable to the path of your service account key file
         if is_running_local: #GH Action was weird with this, so forcing the datatype here
@@ -25,19 +25,29 @@ def connect_bq_client():
                 credentials = service_account.Credentials.from_service_account_info(service_account_key)
                 # Set the environment variable to the path of your service account key file
                 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_key
-        project_id = os.getenv("BQ_PROJECT_ID")
 
         client = bigquery.Client(credentials=credentials, project=project_id)
         return client
 
-def write_df_to_bq_table(df, table_id, dataset_id = 'api_table_uploads', write_disposition_in = "WRITE_TRUNCATE"):
+def write_df_to_bq_table(df, table_id, dataset_id = 'api_table_uploads', write_disposition_in = "WRITE_TRUNCATE", project_id = os.getenv("BQ_PROJECT_ID")):
         schema = []
         # Reset the index of the DataFrame to remove the index column
         df = df.reset_index(drop=True)
 
+        # Check for any flattens to do
+        for column_name, column_type in df.dtypes.items():
+                if column_type == 'object':
+                        # Attempt to flatten nested data if the column contains arrays or dictionaries
+                        try:
+                                df = pu.flatten_nested_data(df, column_name)
+                                continue  # Skip adding the original column to the schema
+                        except ValueError:
+                                continue
         for column_name, column_type in df.dtypes.items():
                 # Map pandas data types to BigQuery data types
-                if column_type == 'float64':
+                if (column_name == 'date' or column_name == 'dt' or column_name.endswith('_dt') or column_name.startswith('dt_')):
+                        bq_data_type = 'DATETIME' #Handle for datefields
+                elif column_type == 'float64':
                         bq_data_type = 'FLOAT64'
                 elif column_type == 'int64':
                         bq_data_type = 'INTEGER'
@@ -47,19 +57,24 @@ def write_df_to_bq_table(df, table_id, dataset_id = 'api_table_uploads', write_d
                         bq_data_type = 'BOOL'
                 elif column_type == 'string':
                         bq_data_type = 'STRING'
-                
+                # elif column_type == 'object':
+                #         # Attempt to flatten nested data if the column contains arrays or dictionaries
+                #         try:
+                #                 df = pu.flatten_nested_data(df, column_name)
+                #                 continue  # Skip adding the original column to the schema
+                #         except ValueError:
+                #                 bq_data_type = 'STRING'
                 else:
                         bq_data_type = 'STRING'
 
                 schema.append(bigquery.SchemaField(column_name, bq_data_type))
 
-        # print(schema)
         # Create a job configuration to overwrite the table
         job_config = bigquery.LoadJobConfig(
                 write_disposition=write_disposition_in,
                 schema=schema
         )
-        client = connect_bq_client()
+        client = connect_bq_client(project_id)
         # Load the DataFrame into BigQuery
         job = client.load_table_from_dataframe(
                 df, f"{dataset_id}.{table_id}", job_config=job_config
@@ -69,3 +84,32 @@ def write_df_to_bq_table(df, table_id, dataset_id = 'api_table_uploads', write_d
         job.result()
 
         print(f"Data loaded successfully to {dataset_id}.{table_id}")
+
+
+# WARNING THE DELETES TABLES
+def delete_bq_table(dataset_id, table_id, project_id=os.getenv("BQ_PROJECT_ID")):
+    """
+    Deletes a table from a BigQuery dataset.
+
+    Args:
+        dataset_id (str): The ID of the dataset containing the table.
+        table_id (str): The ID of the table to be deleted.
+        project_id (str, optional): The ID of the Google Cloud project. If not provided,
+            the project ID from the environment variable GOOGLE_CLOUD_PROJECT will be used.
+
+    Returns:
+        bool: True if the table was deleted successfully, False otherwise.
+    """
+    client = connect_bq_client(project_id = project_id)
+
+    # Get the table reference
+    table_ref = client.dataset(dataset_id).table(table_id)
+
+    try:
+        # Delete the table
+        client.delete_table(table_ref, not_found_ok=True)
+        print(f"Table '{table_id}' deleted successfully from dataset '{dataset_id}'.")
+        return True
+    except Exception as e:
+        print(f"Error deleting table '{table_id}' from dataset '{dataset_id}': {e}")
+        return False
