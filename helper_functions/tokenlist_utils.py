0fd14644e282643384a327df95adc66854e16271
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-07-19 11:59:54 -0400

    mod job
    Former-commit-id: 8d44119ae30524c140fdf5956a2c761ccc38315e

--- helper_functions/tokenlist_utils.py
+++ helper_functions/tokenlist_utils.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import json
 
+def extract_from_extensions(extensions_json, keys):
+    try:
+        extensions = json.loads(extensions_json)
+        return [extensions.get(key) for key in keys]
+    except json.JSONDecodeError:
+        return [None] * len(keys)
+    
 def generate_table_from_tokenlist(data):
         # Create a list of dictionaries, where each dictionary represents a token
         tokens_list = []
@@ -24,20 +31,25 @@ def generate_table_from_tokenlist(data):
 def generate_op_table_from_tokenlist(data):
     # Create a list of dictionaries, where each dictionary represents a token
     tokens_list = []
-    for token in data['tokens']:
-        token_dict = {
-            'chainId': token['chainId'],
-            'address': token['address'],
-            'name': token['name'],
-            'symbol': token['symbol'],
-            'decimals': token['decimals'],
-            'optimismBridgeAddress': token['extensions'].get('optimismBridgeAddress', ''),
-            # **{f'extensions_{k}': v for k, v in token['extensions'].items()}  # Add the extensions as separate columns
-        }
-        # print(token_dict)
-        tokens_list.append(token_dict)
+    for index, token in enumerate(data['tokens']):
+        try:
+            token_dict = {
+                'chainId': token.get('chainId'),
+                'address': token.get('address'),
+                'name': token.get('name'),
+                'symbol': token.get('symbol'),
+                'decimals': token.get('decimals'),
+                'logoURI': token.get('logoURI'),
+                'extensions': json.dumps(token.get('extensions', {}))
+            }
+            tokens_list.append(token_dict)
+        except Exception as e:
+            print(f"Error processing token at index {index}: {e}")
+            print(f"Token data: {token}")
 
     # Create a DataFrame from the list of dictionaries
     df = pd.DataFrame(tokens_list)
+    # Extract 'opListId' and 'opTokenId' from extensions
+    df[['opListId', 'opTokenId']] = df['extensions'].apply(lambda x: extract_from_extensions(x, ['opListId', 'opTokenId'])).tolist()
 
     return df
