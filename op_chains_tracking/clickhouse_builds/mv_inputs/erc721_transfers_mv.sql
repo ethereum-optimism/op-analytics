40dc5c3779d09f5087febf5927b79c39b7102165
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-08-16 18:23:25 -0400

    pushmod for abi method
    Former-commit-id: 66224ca9d1b5cefdbb52d5ff33727058239c8127

---
+++ op_chains_tracking/clickhouse_builds/mv_inputs/erc721_transfers_mv.sql
@@ -0,0 +1,19 @@
+CREATE MATERIALIZED VIEW IF NOT EXISTS {view_name}
+ENGINE = ReplacingMergeTree()
+PARTITION BY toYYYYMM(block_timestamp)
+ORDER BY (chain_id, transaction_hash, log_index, block_number)
+AS
+SELECT
+    transaction_hash,
+    block_timestamp,
+    block_number,
+    log_index,
+    address AS token_contract_address,
+    reinterpretAsUInt256(reverse(unhex(splitByChar(',', topics)[4]))) token_id,
+    '0x' || lower(substring(splitByChar(',', topics)[2], 27, 40)) AS transfer_from_address,
+    '0x' || lower(substring(splitByChar(',', topics)[3], 27, 40)) AS transfer_to_address,
+    chain_id, chain, network
+FROM {chain}_logs
+WHERE splitByChar(',', topics)[1] = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+AND splitByChar(',', topics)[4] != '' --is an nft transfer
+and is_deleted = 0
