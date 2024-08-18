9d427c92df293b361d09bc14cce149c4d8c2c0a0
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-08-18 12:31:07 -0400

    updates
    Former-commit-id: 8947c8598b948e3c7e926c08c76dd363a19cef10

---
+++ op_chains_tracking/clickhouse_builds/mv_inputs/native_eth_transfers_backfill.sql
@@ -0,0 +1,24 @@
+INSERT INTO {view_name}
+-- Native Traces
+SELECT
+
+transaction_hash,
+block_timestamp,
+block_number,
+trace_address, 
+call_type, trace_type,
+value as amount_raw,
+from_address AS transfer_from_address,
+to_address AS transfer_to_address,
+substring(input,1,10) AS input_method_id,
+chain_id, chain, network
+
+FROM {chain}_traces
+
+WHERE value > 0
+AND (call_type not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
+and is_deleted = 0
+and status = 1
+AND block_timestamp BETWEEN '{start_date}' AND '{end_date}'
+
+SETTINGS max_execution_time = 3000
