72ea425904896441428a7923f34e66ad0d3a5da4
chuxinh <chuxin@oplabs.co>
2024-08-20 11:51:05 +1000

    update logic
    Former-commit-id: 6d36cbaaea5887863e97e56229ba9b8b1f9685fd

--- misc_analysis/sql/superbridge_bridgg/insert_across_bridging_txs.sql
+++ misc_analysis/sql/superbridge_bridgg/insert_across_bridging_txs.sql
@@ -1,40 +1,5 @@
 INSERT INTO default.across_bridging_txs
-with across_supported_chains as (
-    select
-        *
-        ,CASE chain_name
-            WHEN 'base' THEN '0x09aea4b2242abc8bb4bb78d537a67a245a7bec64'
-            WHEN 'ethereum' THEN '0x5c7bcd6e7de5423a257d81b442095a1a6ced35c5'
-            WHEN 'lisk' THEN '0x9552a0a6624a23b848060ae5901659cdda1f83f8'
-            WHEN 'mode' THEN '0x3bad7ad0728f9917d1bf08af5782dcbd516cdd96'
-            WHEN 'op' THEN '0x6f26bf09b1c792e3228e5467807a900a503c0281'
-            WHEN 'redstone' THEN '0x28077b47cd03326de7838926a63699849dd4fa87'
-            ELSE NULL
-            END
-        AS spokepool_address
-    from (
-        select
-            chain_name
-            ,display_name
-            ,mainnet_chain_id
-        from "default"."op_stack_chain_metadata"
-        where
-            chain_name in (
-                'base',
-                'lisk',
-                'mode',
-                'op',
-                'redstone'
-            )
 
-        union all
-
-        select
-            'ethereum' as chain_name
-            ,'Ethereum' as display_name
-            ,'1' as mainnet_chain_id
-    )
-)
 select
     x.*
     ,c.chain_name as dst_chain
@@ -49,11 +14,11 @@ from (
         ,'0x' || substring(substring(l.data, 3), 25, 40) as input_token_address
         ,'0x' || substring(substring(l.data, 3), 89, 40) as output_token_address
         ,cast(reinterpretAsUInt64(reverse(unhex(substring(splitByChar(',', l.topics)[2], 3)))) as String) as dst_chain_id
-        ,reinterpretAsUInt64(reverse(unhex(substring(substring(l.data, 3), 129, 64)))) as input_amount
-        ,reinterpretAsUInt64(reverse(unhex(substring(substring(l.data, 3), 193, 64)))) as output_amount
-        ,reinterpretAsUInt64(reverse(unhex(substring(substring(l.data, 3), 257, 64)))) as quote_timestamp
-        ,reinterpretAsUInt64(reverse(unhex(substring(substring(l.data, 3), 321, 64)))) as fill_deadline
-        ,reinterpretAsUInt64(reverse(unhex(substring(substring(l.data, 3), 385, 64)))) as exclusivity_deadline
+        ,reinterpretAsUInt256(reverse(unhex(substring(substring(l.data, 3), 129, 64)))) as input_amount
+        ,reinterpretAsUInt256(reverse(unhex(substring(substring(l.data, 3), 193, 64)))) as output_amount
+        ,reinterpretAsUInt256(reverse(unhex(substring(substring(l.data, 3), 257, 64)))) as quote_timestamp
+        ,reinterpretAsUInt256(reverse(unhex(substring(substring(l.data, 3), 321, 64)))) as fill_deadline
+        ,reinterpretAsUInt256(reverse(unhex(substring(substring(l.data, 3), 385, 64)))) as exclusivity_deadline
         ,'0x' || right(substring(substring(l.data, 3), 449, 64), 40) as recipient_address
         ,'0x' || right(substring(substring(l.data, 3), 513, 64), 40) as relayer_address
         ,t.from_address as depositor_address
@@ -63,7 +28,7 @@ from (
             ELSE null
         END AS integrator
     from "default"."superchain_logs" as l
-    join across_supported_chains as c
+    join default.across_bridge_metadata as c
         on l.chain = c.chain_name
     join "default"."superchain_transactions" as t
         on l.transaction_hash = t.hash
@@ -75,7 +40,7 @@ from (
         and l.network = 'mainnet'
         and t.receipt_status = 1
 ) as x
-join across_supported_chains as c
+join default.across_bridge_metadata as c
     on x.dst_chain_id = c.mainnet_chain_id
 where integrator is not null
 
