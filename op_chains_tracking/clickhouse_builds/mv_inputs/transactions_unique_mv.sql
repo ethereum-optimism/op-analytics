1ad0db84bc72c33d36f42b761f7ee4d835722e1b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-08-26 12:21:04 -0400

    transactions processing matview
    Former-commit-id: a81a03cf937ca0411ea0389671dc13244b3a4ee5

---
+++ op_chains_tracking/clickhouse_builds/mv_inputs/transactions_unique_mv.sql
@@ -0,0 +1,32 @@
+CREATE MATERIALIZED VIEW IF NOT EXISTS {view_name}
+ENGINE = ReplacingMergeTree()
+PARTITION BY toYYYYMM(block_timestamp)
+ORDER BY (chain_id, hash, block_number)
+AS
+
+-- Native Transactions
+SELECT
+        t.*
+
+        , if(gas_price > 0, @gas_fee_sql@, 0) AS gas_fee_eth
+
+        , @byte_length_sql@ AS input_byte_length
+        , @calldata_gas_sql@ AS input_calldata_gas
+        , @estimated_size_sql@ AS estimated_size
+
+        , if(gas_price > 0, CAST(receipt_l1_fee AS Nullable(Float64)) / 1e18, 0) AS l1_contrib_l2_eth_fees_per_day
+        , if(gas_price > 0, CAST(gas_price * t.receipt_gas_used AS Nullable(Float64)) / 1e18, 0) AS l2_contrib_l2_eth_fees_per_day
+
+        , if(gas_price > 0, (cast(@estimated_size_sql@ as Nullable(Float64)) * COALESCE(16*receipt_l1_base_fee_scalar/1e6,receipt_l1_fee_scalar) * cast(receipt_l1_gas_price AS Nullable(Float64))) / 1e18, 0) AS l1_l1gas_contrib_l2_eth_fees_per_day
+        , if(gas_price > 0, (cast(@estimated_size_sql@ as Nullable(Float64)) * receipt_l1_blob_base_fee_scalar/1e6 * cast(receipt_l1_blob_base_fee AS Nullable(Float64))) / 1e18, 0) AS l1_blobgas_contrib_l2_eth_fees_per_day
+
+        , if(gas_price > 0, CAST((base_fee_per_gas) * t.receipt_gas_used AS Nullable(Float64)) / 1e18, 0) AS l2_contrib_l2_eth_fees_base_fee_per_day
+        , if(gas_price > 0, CAST((gas_price - base_fee_per_gas) * t.receipt_gas_used AS Nullable(Float64)) / 1e18, 0) AS l2_contrib_l2_eth_fees_priority_fee_per_day
+
+        , base_fee_per_gas / 1e9 AS l2_base_fee_gwei
+        , if(gas_price > 0 , (gas_price - base_fee_per_gas) / 1e9 , 0 ) AS l2_priority_fee_gwei
+
+FROM {chain}_transactions t
+INNER JOIN {chain}_blocks b 
+        ON t.block_number = b.number
+WHERE t.is_deleted = 0 AND b.is_deleted = 0
