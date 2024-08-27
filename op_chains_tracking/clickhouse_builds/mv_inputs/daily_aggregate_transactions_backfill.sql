4d4f3b06471e8508ac199816eb6432d2d1207c3c
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-08-27 11:20:05 -0400

    fix agg txs
    Former-commit-id: 9ca1d40a5d3430aaeb924ecf627a0b8c31d8707f

--- op_chains_tracking/clickhouse_builds/mv_inputs/daily_aggregate_transactions_backfill.sql
+++ op_chains_tracking/clickhouse_builds/mv_inputs/daily_aggregate_transactions_backfill.sql
@@ -8,46 +8,45 @@ SELECT
     toFixedString(ifNull(to_address, repeat('0', 42)), 42) AS to_address,
 
     countState(*) AS count_transactions_to,
-    countIf(receipt_status = 1) AS count_transactions_to_success,
+    countStateIf(receipt_status = 1) AS count_transactions_to_success,
 
     uniqState(t.block_number) AS count_blocks_to,
     uniqStateIf(t.block_number, receipt_status = 1) AS count_blocks_to_success,
 
-    sum(receipt_gas_used) AS sum_gas_used_to,
-    sumIf(receipt_gas_used, receipt_status = 1) AS sum_gas_used_to_success,
+    sumState(CAST(receipt_gas_used AS UInt128)) AS sum_gas_used_to,
+    sumStateIf(CAST(receipt_gas_used AS UInt128), receipt_status = 1) AS sum_gas_used_to_success,
 
     uniqState(toFixedString(ifNull(from_address, repeat('0', 42)), 42)) AS count_unique_from_addresses_to,
     uniqStateIf(toFixedString(ifNull(from_address, repeat('0', 42)), 42), receipt_status = 1) AS count_unique_from_addresses_to_success,
 
-    SUM(gas_fee_eth) AS sum_eth_gas_fees_to,
-    sumIf(gas_fee_eth, receipt_status = 1) AS sum_eth_gas_fees_to_success,
+    sumState(CAST(gas_fee_eth AS Float64)) AS sum_eth_gas_fees_to,
+    sumStateIf(CAST(gas_fee_eth AS Float64), receipt_status = 1) AS sum_eth_gas_fees_to_success,
+    sumState(CAST(l1_contrib_l2_eth_fees AS Float64)) AS sum_l1_fee_contrib_eth_gas_fees_to,
+    sumStateIf(CAST(l1_contrib_l2_eth_fees AS Float64), receipt_status = 1) AS sum_l1_fee_contrib_eth_gas_fees_to_success,
+    sumState(CAST(l2_contrib_l2_eth_fees AS Float64)) AS sum_l2_fee_contrib_eth_gas_fees_to,
+    sumStateIf(CAST(l2_contrib_l2_eth_fees AS Float64), receipt_status = 1) AS sum_l2_fee_contrib_eth_gas_fees_to_success,
+    sumState(CAST(l2_contrib_l2_eth_fees_base_fee AS Float64)) AS sum_l2_base_contrib_eth_gas_fees_to,
+    sumStateIf(CAST(l2_contrib_l2_eth_fees_base_fee AS Float64), receipt_status = 1) AS sum_l2_base_contrib_eth_gas_fees_to_success,
+    sumState(CAST(l2_contrib_l2_eth_fees_priority_fee AS Float64)) AS sum_l2_priority_contrib_eth_gas_fees_to,
+    sumStateIf(CAST(l2_contrib_l2_eth_fees_priority_fee AS Float64), receipt_status = 1) AS sum_l2_priority_contrib_eth_gas_fees_to_success,
 
-    SUM(input_byte_length) AS sum_input_bytes_to,
-    sumIf(input_byte_length, receipt_status = 1) AS sum_input_bytes_to_success,
+    sumState(CAST(input_byte_length AS Int64)) AS sum_input_bytes_to,
+    sumStateIf(CAST(input_byte_length AS Int64), receipt_status = 1) AS sum_input_bytes_to_success,
 
-    SUM(l1_contrib_l2_eth_fees) AS sum_l1_fee_contrib_eth_gas_fees_to,
-    sumIf(l1_contrib_l2_eth_fees, receipt_status = 1) AS sum_l1_fee_contrib_eth_gas_fees_to_success,
-
-    SUM(l2_contrib_l2_eth_fees) AS sum_l2_fee_contrib_eth_gas_fees_to,
-    sumIf(l2_contrib_l2_eth_fees, receipt_status = 1) AS sum_l2_fee_contrib_eth_gas_fees_to_success,
-
-    SUM(l2_contrib_l2_eth_fees_base_fee) AS sum_l2_base_contrib_eth_gas_fees_to,
-    sumIf(l2_contrib_l2_eth_fees_base_fee, receipt_status = 1) AS sum_l2_base_contrib_eth_gas_fees_to_success,
-    SUM(l2_contrib_l2_eth_fees_priority_fee) AS sum_l2_priority_contrib_eth_gas_fees_to,
-    sumIf(l2_contrib_l2_eth_fees_priority_fee, receipt_status = 1) AS sum_l2_priority_contrib_eth_gas_fees_to_success,
-
-    SUM((t.receipt_gas_used * t.gas_price)/ 1e9)/ SUM(t.receipt_gas_used) AS avg_l2_gas_price_gwei,
-    SUM(CAST(receipt_gas_used AS Float64) * l2_base_fee_gwei)/ SUM(t.receipt_gas_used) AS avg_l2_base_fee_gas_price_gwei,
-    SUM(CAST(receipt_gas_used AS Float64) * l2_priority_fee_gwei)/ SUM(t.receipt_gas_used) AS avg_l2_priority_fee_gas_price_gwei,
-    
-    SUM(COALESCE(receipt_l1_gas_used, input_byte_length) * receipt_l1_gas_price/1e9)/ SUM(COALESCE(receipt_l1_gas_used,input_byte_length)) AS avg_l1_gas_price_gwei
-
+    avgState(CAST((t.receipt_gas_used * t.gas_price) / 1e9 AS Float64)) AS avg_l2_gas_price_gwei,
+    avgState(CAST(receipt_gas_used AS Float64) * CAST(l2_base_fee_gwei AS Float64)) AS avg_l2_base_fee_gas_price_gwei,
+    avgState(CAST(receipt_gas_used AS Float64) * CAST(l2_priority_fee_gwei AS Float64)) AS avg_l2_priority_fee_gas_price_gwei,
+    avgState(CAST(COALESCE(receipt_l1_gas_used, input_byte_length) * receipt_l1_gas_price / 1e9 AS Float64)) AS avg_l1_gas_price_gwei
 
 FROM {chain}_transactions_unique_mv t
-    WHERE gas_price > 0
+WHERE gas_price > 0
     AND is_deleted = 0
     AND t.block_timestamp BETWEEN '{start_date}' AND '{end_date}'
 GROUP BY
-    1,2,3,4,5
+    chain,
+    network,
+    chain_id,
+    dt,
+    to_address
 
 SETTINGS max_execution_time = 3000
