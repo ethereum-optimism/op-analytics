7ca5f8f0387d0b4b7c3fe0ad91ff4d8877804b8e
Pedro M Duarte <pmd323+lithium@gmail.com>
2024-09-24 07:07:27 -0700

    Continue setting up CLI skeleton. Add schema for blocks (#823)
    Co-authored-by: Pedro M Duarte <pedro@oplabs.co>

--- src/op_analytics/cli/main.py
+++ src/op_analytics/cli/main.py
@@ -1,9 +1,31 @@
+import importlib
+import os
+import time
+
+import structlog
 import typer
 
+from op_analytics.cli.mainhelpers import find_apps
 
-def cmd(name: str):
-    print(f"Hello {name}")
+app = typer.Typer(no_args_is_help=True)
+log = structlog.get_logger()
+
+typer.main.get_command_name = lambda name: name
+
+
+# Python modules under the "subcommands" directory are included into the CLI as subcommands.
+# By convention a subcommand module must define an "app" Typer() instance.
+for subapp in find_apps(os.path.join(os.path.dirname(__file__), "subcommands")):
+    start = time.time()
+
+    # Import the moduel and add the "app" instance to the top-level typer app.
+    subapp_module = importlib.import_module(f"op_analytics.cli.subcommands.{subapp}")
+    app.add_typer(subapp_module.app, name=subapp, no_args_is_help=True)
+
+    elapsed = time.time() - start
+    log.info(f"Loaded subcommand: {subapp} ... {elapsed:.2f} seconds")
 
 
 def entrypoint():
-    typer.run(cmd)
+    """This function is used in pyproject.toml as the entrypoint for the opdata CLI."""
+    app()
