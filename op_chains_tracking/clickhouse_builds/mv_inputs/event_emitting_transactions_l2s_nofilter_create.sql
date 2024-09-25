e0cc47cd43420b0132bf1aba3a85084c27af01ba
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-09-25 12:37:38 -0400

    matview updates

---
+++ op_chains_tracking/clickhouse_builds/mv_inputs/event_emitting_transactions_l2s_nofilter_create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE {view_name}
+(
+    block_timestamp         DateTime,
+    block_number            UInt64,
+    transaction_hash        FixedString(66),
+    from_address            FixedString(42),
+    to_address              FixedString(42),
+    tx_value                Float64,
+    l1_gas_fee              Float64,
+    l2_gas_fee              Float64,
+    total_gas_fee           Float64,
+    chain_name              String,
+    insert_time_max         DateTime
+)
+
+ENGINE = ReplacingMergeTree(insert_time_max)
+PARTITION BY toYYYYMM(block_timestamp)
+ORDER BY (block_timestamp, block_number, transaction_hash, chain_name)
