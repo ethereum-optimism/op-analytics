3e7790418f6c216b0458e0ea160592a49d942cb0
Pedro M Duarte <pmd323+lithium@gmail.com>
2024-09-27 14:17:46 -0700

    Some progress on dbt source setup (#838)
    * Add hourly workflow using opdata cli
    
    * Save progress on dbt source setup
    
    ---------
    
    Co-authored-by: Pedro M Duarte <pedro@oplabs.co>
    Former-commit-id: 05a27f090ce91928bb7f90d2f944cefc8f98332c

--- src/op_analytics/cli/subcommands/chains/app.py
+++ src/op_analytics/cli/subcommands/chains/app.py
@@ -1,28 +1,22 @@
 import typer
-from typing_extensions import Annotated
+from op_coreutils.logger import LOGGER
+
 
 from op_analytics.cli.subcommands.chains import chain_metadata
-from op_coreutils.storage.gcs import gcs_upload_csv
+from op_analytics.cli.subcommands.chains import goldsky_dbt_sources
+from op_analytics.cli.subcommands.chains import superchain_dbt_sources
+from op_analytics.cli.subcommands.chains import dbt_docs
+
+log = LOGGER.get_logger()
 
 app = typer.Typer(help="Chain related utilities.")
 
 
+app.command(name="upload_metadata")(chain_metadata.upload_metadata)
+app.command(name="customize_dbt_docs")(dbt_docs.customize)
+
+
 @app.command()
-def clean_and_upload_chain_metadata(
-    path: Annotated[
-        str, typer.Argument(help="Path to local file that has the raw chain metadata.")
-    ],
-):
-    """Upload raw chain metadata csv file.
-
-
-    The chain_metadata_raw.csv file is maintained manually by the OP Labs team. This function
-    accepts a local CSV file with raw chain metadata. It loads the data, cleans it up and uploads
-    it to GCS, Dune, Clickhouse and BigQuery.
-    """
-    import polars as pl
-
-    with open(path, "r") as fcsv:
-        raw_df = pl.scan_csv(fcsv)
-        clean_df = chain_metadata.clean(raw_df).collect()
-        gcs_upload_csv("op_chains_tracking/chain_metadata.csv", clean_df)
+def generate_dbt_sources():
+    goldsky_dbt_sources.generate()
+    superchain_dbt_sources.generate()
--- src/op_analytics/cli/subcommands/chains/chain_metadata.py
+++ src/op_analytics/cli/subcommands/chains/chain_metadata.py
@@ -1,14 +1,53 @@
 import polars as pl
-
+import typer
+from op_coreutils.logger import LOGGER
+from op_coreutils.path import repo_path
+from op_coreutils.storage.gcs import gcs_upload_csv
 from polars import datatypes
 from polars.functions.col import Col
+from typing_extensions import Annotated
+
+log = LOGGER.get_logger()
 
 
 OP_CHAIN = "OP Chain"
 OP_FORK = "OP Stack fork"
 
 
-def clean(raw_df: pl.DataFrame):
+DEFAULT_CHAIN_METADATA_LOCATION = repo_path("op_chains_tracking/inputs/chain_metadata_raw.csv")
+
+
+def load_chain_metadata(path: str | None = None) -> pl.DataFrame:
+    path = path or DEFAULT_CHAIN_METADATA_LOCATION
+
+    with open(path, "r") as fcsv:
+        raw_df = pl.scan_csv(fcsv)
+        log.info(f"Loaded OP chains metadata from {path}")
+        return _clean(raw_df).collect()
+
+
+def goldsky_chains(path: str | None = None):
+    df = load_chain_metadata(path)
+    return sorted(df.filter(pl.col("oplabs_db_schema").is_not_null())["oplabs_db_schema"].to_list())
+
+
+def upload_metadata(
+    path: Annotated[
+        str, typer.Argument(help="Path to local file that has the raw chain metadata.")
+    ],
+):
+    """Upload raw chain metadata csv file.
+
+
+    The chain_metadata_raw.csv file is maintained manually by the OP Labs team. This function
+    accepts a local CSV file with raw chain metadata. It loads the data, cleans it up and uploads
+    it to GCS, Dune, Clickhouse and BigQuery.
+    """
+    clean_df = load_chain_metadata(path)
+    gcs_upload_csv("op_chains_tracking/chain_metadata.csv", clean_df)
+
+
+def _clean(raw_df: pl.DataFrame):
     """Clean and enrich the raw chain metadata.
 
     The enriched columns are:
