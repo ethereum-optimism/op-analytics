48a90182c5461c15b875013daa19a3162e9f57ff
Pedro M Duarte <pmd323+lithium@gmail.com>
2024-10-07 10:50:31 -0700

    L2Beat API uploads (#867)
    * Save work on l2beat api pulls
    
    * Daily api uploads workflow
    
    * Update workflow branch
    
    * Do not fail if .env does not exist
    
    * Attempt to setup gcp credentials
    
    * Make workflow daily
    
    * Add docstring
    
    ---------
    
    Co-authored-by: Pedro M Duarte <pedro@oplabs.co>

--- packages/op-coreutils/src/op_coreutils/time.py
+++ packages/op-coreutils/src/op_coreutils/time.py
@@ -11,3 +11,7 @@ def now_seconds():
     """Returns a naive datetime object with the current UTC timestamp in seconds resolution."""
 
     return now().replace(microsecond=0)
+
+
+def now_dt() -> str:
+    return now_seconds().strftime("%Y-%m-%d")
