17c2a56689cca48e5409c553b42a0901cc982fd6
Pedro M Duarte <pedro@oplabs.co>
2024-10-15 11:30:15 -0400

    Create stubs for Agora daily API pull

--- src/op_analytics/cli/subcommands/pulls/app.py
+++ src/op_analytics/cli/subcommands/pulls/app.py
@@ -2,6 +2,7 @@ import typer
 from op_coreutils.logger import structlog
 from op_analytics.cli.subcommands.pulls.l2beat import pull as l2beat_pull
 from op_analytics.cli.subcommands.pulls.defillama import pull_stables as dfl_pull_stables
+from op_analytics.cli.subcommands.pulls.agora import agora_pull
 
 log = structlog.get_logger()
 
@@ -14,7 +15,14 @@ def l2beat():
     """Pull data from L2 beat."""
     l2beat_pull()
 
+
 @app.command()
 def dfl_stables():
     """Pull stablecoin data from Defillama."""
     dfl_pull_stables()
+
+
+@app.command()
+def agora():
+    """Pull data from Agora."""
+    agora_pull()
