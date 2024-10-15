a3da4f5b4a35005bdd9a42222164dec64ab9dd17
Pedro M Duarte <pmd323+lithium@gmail.com>
2024-10-15 14:47:21 -0700

    Add traces_v1 core dataset (#883)
    Co-authored-by: Pedro M Duarte <pedro@oplabs.co>

--- packages/op-datasets/src/op_datasets/schemas/__init__.py
+++ packages/op-datasets/src/op_datasets/schemas/__init__.py
@@ -2,18 +2,21 @@ from .core import CoreDataset
 from .blocks.v1 import BLOCKS_V1_SCHEMA
 from .transactions.v1 import TRANSACTIONS_V1_SCHEMA
 from .logs.v1 import LOGS_V1_SCHEMA
+from .traces.v1 import TRACES_V1_SCHEMA
 
 
 ONCHAIN_CORE_DATASETS = {
     "blocks_v1": BLOCKS_V1_SCHEMA,
     "transactions_v1": TRANSACTIONS_V1_SCHEMA,
     "logs_v1": LOGS_V1_SCHEMA,
+    "traces_v1": TRACES_V1_SCHEMA,
 }
 
 ONCHAIN_CURRENT_VERSION = {
     "blocks": "blocks_v1",
     "transactions": "transactions_v1",
     "logs": "logs_v1",
+    "traces": "traces_v1",
 }
 
 
--- src/op_analytics/cli/subcommands/chains/app.py
+++ src/op_analytics/cli/subcommands/chains/app.py
@@ -4,7 +4,9 @@ import op_datasets
 import op_datasets.rpcs
 import typer
 from op_coreutils.logger import structlog
+from op_datasets.processing.blockrange import BlockRange
 from op_datasets.processing.execute import execute
+from op_datasets.schemas import resolve_core_dataset
 from typing_extensions import Annotated
 
 log = structlog.get_logger()
@@ -36,6 +38,24 @@ def get_receipts(chain: str, tx_hashes: list[str]):
     """Get transaction receipts."""
     txs = op_datasets.rpcs.get_receipts(chain, tx_hashes)
     print(json.dumps(txs, indent=2))
+
+
+@app.command()
+def goldsky_sql(
+    chain: Annotated[str, typer.Argument(help="L2 chain name")],
+    block_spec: Annotated[str, typer.Argument(help="Range of blocks to be ingested.")],
+    dataset_name: str,
+):
+    """Helper command to show the SQL we use to query Goldsky Clickhouse for a given block range."""
+    block_range = BlockRange.from_spec(block_spec)
+    dataset = resolve_core_dataset(dataset_name)
+
+    sql = dataset.goldsky_sql(
+        source_table=f"{chain}_{dataset.goldsky_table}",
+        where=block_range.filter(number_column=dataset.block_number_col),
+    )
+
+    print(sql)
 
 
 @app.command()
