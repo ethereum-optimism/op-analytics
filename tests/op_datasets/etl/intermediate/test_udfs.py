eb4b8c2acb17c0bfc5c28d04ca136ad4ace42169
Pedro M Duarte <pedro@oplabs.co>
2024-10-23 13:12:33 -0400

    Add duckdb common functions to be shared across intermediate model definitions

---
+++ tests/op_datasets/etl/intermediate/test_udfs.py
@@ -0,0 +1,51 @@
+from decimal import Decimal
+
+from op_datasets.etl.intermediate.udfs import (
+    create_duckdb_macros,
+    Expression,
+    safe_div,
+    wei_to_eth,
+    wei_to_gwei,
+    to_sql,
+)
+
+
+def test_macros():
+    client = create_duckdb_macros()
+
+    client.sql("""
+    CREATE TABLE test_macros AS 
+    SELECT 
+        100::BIGINT AS gas_price,
+        200::BIGINT AS receipt_gas_used,
+        0 AS zero,
+        50 as fifty
+    """)
+
+    # Use the Expression class to create the sql.
+    exprs = [
+        Expression(alias="ans_eth", sql_expr=wei_to_eth("gas_price * receipt_gas_used")),
+        Expression(alias="ans_gwei", sql_expr=wei_to_gwei("gas_price * receipt_gas_used")),
+        Expression(alias="ans_division_ok", sql_expr=safe_div("receipt_gas_used", "fifty")),
+        Expression(alias="ans_division_err", sql_expr=safe_div("receipt_gas_used", "zero")),
+    ]
+    result1 = client.sql(f"""
+    SELECT gas_price * receipt_gas_used, {to_sql(exprs)} FROM test_macros
+    """)
+    actual1 = result1.fetchall()
+    expected = [(20000, Decimal("2.00000E-14"), Decimal("0.0000200000"), 4.0, None)]
+
+    assert actual1 == expected
+
+    # Use raw sql.
+    result2 = client.sql("""
+    SELECT
+        gas_price * receipt_gas_used,
+        wei_to_eth(gas_price * receipt_gas_used) AS ans_eth,
+        wei_to_gwei(gas_price * receipt_gas_used) AS ans_gwei,
+        safe_div(receipt_gas_used, fifty) AS ans_division_ok,
+        safe_div(receipt_gas_used, zero) AS ans_division_err
+    FROM test_macros
+    """)
+    actual2 = result2.fetchall()
+    assert actual2 == expected
