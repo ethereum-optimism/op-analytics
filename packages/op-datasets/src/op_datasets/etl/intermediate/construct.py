8e8fae317f49d8ca2c04466e9733ca1d6a8a9881
Pedro M Duarte <pmd323+lithium@gmail.com>
2024-11-11 17:01:28 -0500

    More updates for writing intermediate models (#997)
    Former-commit-id: 241fbfb1d2959efea5e758baef38be5d152bb588

--- packages/op-datasets/src/op_datasets/etl/intermediate/construct.py
+++ packages/op-datasets/src/op_datasets/etl/intermediate/construct.py
@@ -23,11 +23,12 @@ log = structlog.get_logger()
 def construnct_intermediate_model_batches(
     chains: list[str],
     range_spec: str,
+    read_from: DataLocation,
 ) -> list[DataReader]:
     return construct_input_batches(
         chains=chains,
         range_spec=range_spec,
-        read_from=DataLocation.GCS,
+        read_from=read_from,
         markers_table=INGESTION_MARKERS_TABLE,
         dataset_names=INGESTION_DATASETS,
     )
@@ -49,6 +50,7 @@ def construct_tasks(
     batches: list[DataReader] = construnct_intermediate_model_batches(
         chains=chains,
         range_spec=range_spec,
+        read_from=read_from,
     )
 
     tasks = []
@@ -59,14 +61,18 @@ def construct_tasks(
             for dataset in REGISTERED_INTERMEDIATE_MODELS[model].expected_output_datasets:
                 full_model_name = f"{model}/{dataset}"
 
+                datestr = batch.dateval.strftime("%Y%m%d")
+                chain = batch.chain
+                marker_path = SinkMarkerPath(f"{datestr}/{chain}/{model}/{dataset}")
+
                 expected_outputs[full_model_name] = ExpectedOutput(
                     dataset_name=full_model_name,
                     root_path=SinkOutputRootPath(f"intermediate/{full_model_name}"),
                     file_name="out.parquet",
-                    marker_path=SinkMarkerPath(full_model_name),
+                    marker_path=marker_path,
                     process_name="default",
                     additional_columns=dict(
-                        mode_name=model,
+                        model_name=model,
                     ),
                     additional_columns_schema=[
                         pa.field("chain", pa.string()),
--- tests/op_coreutils/partitioned/test_breakout.py
+++ tests/op_coreutils/partitioned/test_breakout.py
@@ -30,49 +30,63 @@ def test_breakout_partitions():
         outputs.append(part)
         assert part.df.columns == ["c"]
 
-    actual = sorted([_.meta for _ in outputs], key=lambda x: x.partitions_path)
+    actual = []
+    for _ in outputs:
+        actual.append(
+            dict(
+                meta=_.meta,
+                full_path=_.meta.full_path("warehouse", "myfile.parquet"),
+                nu_rows=len(_.df),
+            )
+        )
 
+    actual.sort(key=lambda x: x["meta"].partitions_path)
     assert actual == [
-        OutputPartMeta(
-            partitions=[
-                KeyValue(key="dt", value="2024-01-01"),
-                KeyValue(key="chain", value="base"),
-            ],
-            row_count=2,
-        ),
-        OutputPartMeta(
-            partitions=[
-                KeyValue(key="dt", value="2024-01-01"),
-                KeyValue(key="chain", value="op"),
-            ],
-            row_count=2,
-        ),
-        OutputPartMeta(
-            partitions=[
-                KeyValue(key="dt", value="2024-01-02"),
-                KeyValue(key="chain", value="base"),
-            ],
-            row_count=2,
-        ),
-        OutputPartMeta(
-            partitions=[
-                KeyValue(key="dt", value="2024-01-03"),
-                KeyValue(key="chain", value="op"),
-            ],
-            row_count=1,
-        ),
+        {
+            "meta": OutputPartMeta(
+                partitions=[
+                    KeyValue(key="dt", value="2024-01-01"),
+                    KeyValue(key="chain", value="base"),
+                ],
+                row_count=2,
+            ),
+            "full_path": "warehouse/dt=2024-01-01/chain=base/myfile.parquet",
+            "nu_rows": 2,
+        },
+        {
+            "meta": OutputPartMeta(
+                partitions=[
+                    KeyValue(key="dt", value="2024-01-01"),
+                    KeyValue(key="chain", value="op"),
+                ],
+                row_count=2,
+            ),
+            "full_path": "warehouse/dt=2024-01-01/chain=op/myfile.parquet",
+            "nu_rows": 2,
+        },
+        {
+            "meta": OutputPartMeta(
+                partitions=[
+                    KeyValue(key="dt", value="2024-01-02"),
+                    KeyValue(key="chain", value="base"),
+                ],
+                row_count=2,
+            ),
+            "full_path": "warehouse/dt=2024-01-02/chain=base/myfile.parquet",
+            "nu_rows": 2,
+        },
+        {
+            "meta": OutputPartMeta(
+                partitions=[
+                    KeyValue(key="dt", value="2024-01-03"),
+                    KeyValue(key="chain", value="op"),
+                ],
+                row_count=1,
+            ),
+            "full_path": "warehouse/dt=2024-01-03/chain=op/myfile.parquet",
+            "nu_rows": 1,
+        },
     ]
-
-    paths = sorted([_.meta.full_path("warehouse", "myfile.parquet") for _ in outputs])
-    assert paths == [
-        "warehouse/dt=2024-01-01/chain=base/myfile.parquet",
-        "warehouse/dt=2024-01-01/chain=op/myfile.parquet",
-        "warehouse/dt=2024-01-02/chain=base/myfile.parquet",
-        "warehouse/dt=2024-01-03/chain=op/myfile.parquet",
-    ]
-
-    sizes = [len(_.df) for _ in outputs]
-    assert sizes == [2, 2, 2, 1]
 
 
 def test_breakout_partitions_empty():
@@ -101,17 +115,27 @@ def test_breakout_partitions_empty():
         outputs.append(part)
         assert part.df.columns == ["c"]
 
-    actual = sorted([_.meta for _ in outputs], key=lambda x: x.partitions_path)
-
-    assert actual == [
-        OutputPartMeta(
-            partitions=[KeyValue(key="chain", value="op"), KeyValue(key="dt", value="2023-10-30")],
-            row_count=0,
+    actual = []
+    for _ in outputs:
+        actual.append(
+            dict(
+                meta=_.meta,
+                full_path=_.meta.full_path("warehouse", "myfile.parquet"),
+                nu_rows=len(_.df),
+            )
         )
+
+    actual.sort(key=lambda x: x["meta"].partitions_path)
+    assert actual == [
+        {
+            "meta": OutputPartMeta(
+                partitions=[
+                    KeyValue(key="chain", value="op"),
+                    KeyValue(key="dt", value="2023-10-30"),
+                ],
+                row_count=0,
+            ),
+            "full_path": "warehouse/chain=op/dt=2023-10-30/myfile.parquet",
+            "nu_rows": 0,
+        }
     ]
-
-    paths = [_.meta.full_path("warehouse", "myfile.parquet") for _ in outputs]
-    assert paths == ["warehouse/chain=op/dt=2023-10-30/myfile.parquet"]
-
-    sizes = [len(_.df) for _ in outputs]
-    assert sizes == [0]
