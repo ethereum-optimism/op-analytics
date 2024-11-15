c68f568b7632de53ec051c4c554f63fbd9bc6826
Pedro M Duarte <pedro@oplabs.co>
2024-11-15 09:31:46 -0500

    Load vault contents in kubernetes runtime

--- src/op_analytics/coreutils/env/vault.py
+++ src/op_analytics/coreutils/env/vault.py
@@ -1,17 +1,43 @@
 import json
-import os
 import base64
 
+from op_analytics.coreutils.env.aware import is_k8s
 from op_analytics.coreutils.logger import structlog
+from op_analytics.coreutils.path import repo_path
 
 log = structlog.get_logger()
 
 _STORE: dict | None = None
 
 
-def load_encoded(var_name) -> dict:
+def load_dotenv() -> dict:
+    dotenv_path = repo_path(".env")
+
+    if dotenv_path is None:
+        log.error("Did not find .env. No env vars will be loaded.")
+        return {}
+
+    result = {}
+    with open(dotenv_path, "r") as fobj:
+        for line in fobj:
+            key, val = line.split("=", maxsplit=1)
+            result[key] = val.strip()
+
+    return result
+
+
+def load_vault() -> dict:
     default: bytes = base64.b64encode("{}".encode())
-    result = json.loads(base64.b64decode(os.environ.get(var_name, default)).decode())
+
+    if is_k8s():
+        with open("/var/secrets/op-analytics-vault.txt", "r") as fobj:
+            raw = fobj.read()
+            result = json.loads(base64.b64decode(raw).decode())
+    else:
+        dotenv = load_dotenv()
+        var_name = "OP_ANALYTICS_VAULT"
+        result = json.loads(base64.b64decode(dotenv.get(var_name, default)).decode())
+
     if not isinstance(result, dict):
         # FOR SECURITY DO NOT PRINT THE LOADED "result".
         raise ValueError(f"was expecting a dictionary at {var_name}")
@@ -26,7 +52,7 @@ def init() -> None:
         # Only initialize  once.
         return
 
-    data = load_encoded("OP_ANALYTICS_VAULT")
+    data = load_vault()
 
     _STORE = {}
     for key, val in data.items():
