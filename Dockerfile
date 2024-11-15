ef93129c7868d66e38f009cd467048d2e712561b
Pedro M Duarte <pedro@oplabs.co>
2024-11-14 21:56:43 -0500

    Start setting up docker + k8s for scheduling

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# syntax=docker/dockerfile:1.9
+FROM python:3.12-slim-bookworm
+
+
+# Install tiny (add more system packages here if needed)
+RUN <<EOT
+apt-get update -qy
+apt-get install -qyy \
+    -o APT::Install-Recommends=false \
+    -o APT::Install-Suggests=false \
+    tini
+
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+EOT
+
+
+# Install the package
+COPY dist/ /_dist/
+RUN --mount=type=cache,target=/root/.cache <<EOT
+cd /_dist
+pip install op_analytics-24.11.140-py3-none-any.whl
+EOT
+
+
+# Don't run as root.
+RUN <<EOT
+groupadd -r app
+useradd -r -d /app -g app -N app
+EOT
+
+
+COPY docker-entrypoint.sh /app/docker-entrypoint.sh
+ENTRYPOINT ["tini", "-v", "--", "/app/docker-entrypoint.sh"]
+# See <https://hynek.me/articles/docker-signals/>.
+STOPSIGNAL SIGINT
---
+++ docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Exit immediately if any command exits with a non-zero status
+set -e
+
+# Function to print usage
+usage() {
+    echo "Usage: $0 {healthcheck|ingest|intermediate}"
+    exit 1
+}
+
+# Check if an argument is passed
+if [ -z "$1" ]; then
+    usage
+fi
+
+# Switch based on the input argument
+case "$1" in
+    healthcheck)
+        echo "Checking docker container health..."
+        exec opdata chains health
+        ;;
+    ingest)
+        echo "Executing Ingestion..."
+        exec env GOLDSKY_FINAL=true uv run opdata chains ingest_blocks ALL m2days --read_from goldsky --write_to gcs
+        ;;
+    intermediate)
+        echo "Executing Intermediate..."
+        exec opdata chains health
+        ;;
+    *)
+        echo "Invalid argument: $1"
+        usage
+        ;;
+esac
