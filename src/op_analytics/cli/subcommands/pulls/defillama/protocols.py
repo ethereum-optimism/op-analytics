b9b43fc9447e785d2f9baa4da08f72d4953db4c6
chuck <chuck@oplabs.co>
2024-11-20 10:48:02 -0500

    flip create flag

--- src/op_analytics/cli/subcommands/pulls/defillama/protocols.py
+++ src/op_analytics/cli/subcommands/pulls/defillama/protocols.py
@@ -65,7 +65,7 @@ def pull_protocol_tvl(pull_protocols: list[str] | None = None) -> DefillamaProto
         dataset=BQ_DATASET,
         table_name=PROTOCOL_TVL_DATA_TABLE,
         unique_keys=["protocol_slug", "chain", "date"],
-        create_if_not_exists=True,  # Set to True on first run
+        create_if_not_exists=False,  # Set to True on first run
     )
 
     upsert_unpartitioned_table(
@@ -73,7 +73,7 @@ def pull_protocol_tvl(pull_protocols: list[str] | None = None) -> DefillamaProto
         dataset=BQ_DATASET,
         table_name=PROTOCOL_TOKEN_TVL_DATA_TABLE,
         unique_keys=["protocol_slug", "chain", "date", "token"],
-        create_if_not_exists=True,  # Set to True on first run
+        create_if_not_exists=False,  # Set to True on first run
     )
 
     return DefillamaProtocols(
