a242da3423ca322153b5b48e7bf3213e57e529d5
chuxinh <chuxin@oplabs.co>
2024-11-21 16:08:08 +1100

    format

---
+++ src/op_analytics/datapipeline/etl/intermediate/models/creation_traces.py
@@ -0,0 +1,21 @@
+import duckdb
+
+from op_analytics.datapipeline.etl.intermediate.querybuilder import TemplatedSQLQuery
+from op_analytics.datapipeline.etl.intermediate.registry import register_model
+from op_analytics.datapipeline.etl.intermediate.types import NamedRelations
+
+
+@register_model(
+    input_datasets=["traces", "transactions"],
+    expected_outputs=["creation_traces_v1"],
+    duckdb_views=[
+        TemplatedSQLQuery(
+            template_name="creation_traces",
+            context={},
+        ),
+    ],
+)
+def creation_traces(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
+    return {
+        "creation_traces_v1": duckdb_client.sql("SELECT * FROM creation_traces"),
+    }
