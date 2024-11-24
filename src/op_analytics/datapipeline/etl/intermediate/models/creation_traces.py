e4b18827e48a2e4fed27d298b54f51aaa45acede
Pedro M Duarte <pedro@oplabs.co>
2024-11-23 21:46:02 -0500

    Use view instead of SELECT *

--- src/op_analytics/datapipeline/etl/intermediate/models/creation_traces.py
+++ src/op_analytics/datapipeline/etl/intermediate/models/creation_traces.py
@@ -17,5 +17,5 @@ from op_analytics.datapipeline.etl.intermediate.types import NamedRelations
 )
 def creation_traces(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
     return {
-        "creation_traces_v1": duckdb_client.sql("SELECT * FROM creation_traces"),
+        "creation_traces_v1": duckdb_client.view("creation_traces"),
     }
