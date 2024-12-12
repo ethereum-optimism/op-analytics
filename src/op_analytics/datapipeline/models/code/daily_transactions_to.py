ebeb470bd76903258a17f183dd70ccfdb4259e8b
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-12-12 17:35:38 -0500

    push example aggregation

---
+++ src/op_analytics/datapipeline/models/code/daily_transactions_to.py
@@ -0,0 +1,26 @@
+import duckdb
+
+from op_analytics.datapipeline.models.compute.querybuilder import TemplatedSQLQuery
+from op_analytics.datapipeline.models.compute.registry import register_model
+from op_analytics.datapipeline.models.compute.types import NamedRelations
+
+
+@register_model(
+    input_datasets=["intermediate/enriched_transactions_v1"],
+    expected_outputs=["daily_transactions_to_v1"],
+    # TODO: Uncomment if we do this as a view (or some element as a view)
+    # auxiliary_views=[
+    #     TemplatedSQLQuery(
+    #         template_name="daily_transactions_to",
+    #         context={},
+    #     ),
+    # ],
+)
+def daily_transactions_to(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
+    return {
+        "daily_transactions_to_v1": duckdb_client.view(
+            """
+            TODO: AGGREGATION CODE
+            """
+        ),
+    }
