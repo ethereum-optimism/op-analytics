f7e6345828e8bd7f275295a5dca49c8fb0188059
Pedro M Duarte <pmd323+lithium@gmail.com>
2024-12-12 13:57:27 -0500

    Eta logger fixes (#1163)

--- src/op_analytics/coreutils/logger.py
+++ src/op_analytics/coreutils/logger.py
@@ -94,16 +94,16 @@ def human_rows(num_rows):
 
 def human_interval(num_seconds: int) -> str:
     if num_seconds < 120:
-        return f"{num_seconds} secs"
+        return f"{num_seconds}s"
     elif num_seconds < 7200:
         num_minutes = num_seconds / 60
-        return f"{num_minutes:.1f} mins"
+        return f"{num_minutes:.1f}m"
     elif num_seconds < 24 * 3600 * 3:
         num_hours = num_seconds / 3600
-        return f"{num_hours:.1f} hrs"
+        return f"{num_hours:.1f}h"
     else:
         num_days = num_seconds / (24 * 3600)
-        return f"{num_days:.1f} days"
+        return f"{num_days:.1f}d"
 
 
 def memory_usage():
