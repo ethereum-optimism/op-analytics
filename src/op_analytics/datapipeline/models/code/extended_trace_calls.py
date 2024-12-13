ccc8ed931158f82a7ffc852bcb161eab0997b236
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-12-12 23:00:02 -0500

    migrate to existing transaction_fees and rename

--- src/op_analytics/datapipeline/models/code/extended_trace_calls.py
+++ src/op_analytics/datapipeline/models/code/extended_trace_calls.py
@@ -6,7 +6,7 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 
 @register_model(
-    input_datasets=["ingestion/traces_v1", "enriched_transactions_v1"],
+    input_datasets=["ingestion/traces_v1", "extended_transactions_fees_v1"],
     expected_outputs=["enriched_trace_calls_v1"],
     auxiliary_views=[
         TemplatedSQLQuery(
--- src/op_analytics/datapipeline/models/code/extended_transactions_fees.py
+++ src/op_analytics/datapipeline/models/code/extended_transactions_fees.py
@@ -6,16 +6,16 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 
 @register_model(
-    input_datasets=["ingestion/blocks_v1", "ingestion/transactions_v1", "extended_transactions_fees"],
-    expected_outputs=["summary_v1"],
+    input_datasets=["ingestion/logs_v1"],
+    expected_outputs=["extended_transactions_fees.sql_v1"],
     auxiliary_views=[
         TemplatedSQLQuery(
-            template_name="daily_address_summary",
+            template_name="extended_transactions_fees",
             context={},
         ),
     ],
 )
-def daily_address_summary(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
+def extended_transactions_fees(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
     return {
-        "summary_v1": duckdb_client.view("daily_address_summary"),
+        "extended_transactions_fees_v1": duckdb_client.view("extended_transactions_fees"),
     }
