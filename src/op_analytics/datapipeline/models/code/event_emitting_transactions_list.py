7905ab0922eb638b29df9eb5d269070c0a54dc5f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-12-13 00:19:21 -0500

    fix eet

--- src/op_analytics/datapipeline/models/code/event_emitting_transactions_list.py
+++ src/op_analytics/datapipeline/models/code/event_emitting_transactions_list.py
@@ -6,7 +6,7 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 
 @register_model(
-    input_datasets=["ingestion/logs_v1"],
+    input_datasets=["ingestion/logs_v1", "ingestion/transactions_v1"],
     expected_outputs=["event_emitting_transactions_list_v1"],
     auxiliary_views=[
         TemplatedSQLQuery(
@@ -17,5 +17,7 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 )
 def event_emitting_transactions_list(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
     return {
-        "event_emitting_transactions_list_v1": duckdb_client.view("event_emitting_transactions_list"),
+        "event_emitting_transactions_list_v1": duckdb_client.view(
+            "event_emitting_transactions_list"
+        ),
     }
