65926827b2b03b95138a0a11f929cfd1bc2a883c
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-12-13 07:24:51 -0500

    simplify views

--- src/op_analytics/datapipeline/models/code/refined_transactions_fees.py
+++ src/op_analytics/datapipeline/models/code/refined_transactions_fees.py
@@ -6,9 +6,17 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 
 @register_model(
-    input_datasets=["ingestion/logs_v1", "event_emitting_transactions_v1"],
+    input_datasets=[
+        "ingestion/transactions_v1",
+        "ingestion/blocks_v1",
+        "intermediate/event_emitting_transactions_v1",
+    ],
     expected_outputs=["refined_transactions_fees_v1"],
     auxiliary_views=[
+        TemplatedSQLQuery(
+            template_name="base_transactions_fees",
+            context={},
+        ),
         TemplatedSQLQuery(
             template_name="refined_transactions_fees",
             context={},
