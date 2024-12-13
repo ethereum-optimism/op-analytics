dd03a2a2c6e96de9987261868ec57b0c989d3b45
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-12-12 22:17:51 -0500

    resolve sources

--- src/op_analytics/datapipeline/models/code/enriched_trace_calls.py
+++ src/op_analytics/datapipeline/models/code/enriched_trace_calls.py
@@ -6,13 +6,9 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 
 @register_model(
-    input_datasets=["ingestion/traces_v1", "ingestion/transactions_v1", "ingestion/blocks_v1"],
+    input_datasets=["ingestion/traces_v1", "enriched_transactions_v1"],
     expected_outputs=["enriched_trace_calls_v1"],
     auxiliary_views=[
-        TemplatedSQLQuery(
-            template_name="enriched_transactions",
-            context={},
-        ),
         TemplatedSQLQuery(
             template_name="enriched_trace_calls",
             context={},
--- src/op_analytics/datapipeline/models/code/enriched_transactions.py
+++ src/op_analytics/datapipeline/models/code/enriched_transactions.py
@@ -6,13 +6,9 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 
 @register_model(
-    input_datasets=["ingestion/transactions_v1", "ingestion/blocks_v1"],
+    input_datasets=["ingestion/transactions_v1", "ingestion/blocks_v1", "event_emitting_transactions_list_v1"],
     expected_outputs=["enriched_transactions_v1"],
     auxiliary_views=[
-        TemplatedSQLQuery(
-            template_name="event_emitting_transactions",
-            context={},
-        ),
         TemplatedSQLQuery(
             template_name="enriched_transactions",
             context={},
