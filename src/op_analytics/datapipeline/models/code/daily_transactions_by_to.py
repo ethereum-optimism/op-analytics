cc12c72078e574a3f9d7530e3e01a9377aab303a
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2024-12-12 20:25:53 -0500

    mod

--- src/op_analytics/datapipeline/models/code/daily_transactions_by_to.py
+++ src/op_analytics/datapipeline/models/code/daily_transactions_by_to.py
@@ -7,18 +7,18 @@ from op_analytics.datapipeline.models.compute.types import NamedRelations
 
 @register_model(
     input_datasets=["intermediate/enriched_transactions_v1"],
-    expected_outputs=["daily_transactions_to_v1"],
+    expected_outputs=["daily_transactions_by_to_v1"],
     # TODO: Uncomment if we do this as a view (or some element as a view)
     # auxiliary_views=[
     #     TemplatedSQLQuery(
-    #         template_name="daily_transactions_to",
+    #         template_name="daily_transactions_by_to",
     #         context={},
     #     ),
     # ],
 )
-def daily_transactions_to(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
+def daily_transactions_by_to(duckdb_client: duckdb.DuckDBPyConnection) -> NamedRelations:
     return {
-        "daily_transactions_to_v1": duckdb_client.view(
+        "daily_transactions_by_to_v1": duckdb_client.view(
             """
             TODO: AGGREGATION CODE
             """
