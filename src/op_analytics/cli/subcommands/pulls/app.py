862c9f7077fc2aea9a3412bc5c96ad730ae6ee03
Dennis <dionisis@dioptx.com>
2025-01-07 12:50:42 +0200

    create a github folder

--- src/op_analytics/cli/subcommands/pulls/app.py
+++ src/op_analytics/cli/subcommands/pulls/app.py
@@ -13,7 +13,7 @@ from .defillama.historical_chain_tvl import pull_historical_chain_tvl
 from .defillama.protocols import pull_protocol_tvl
 from .defillama.stablecoins import pull_stablecoins
 from .defillama.dex_volume_fees_revenue import pull_dex_volume, pull_fees, pull_revenue
-from .github_analytics import pull_github_analytics
+from .github.github_analytics import pull_github_analytics
 from .growthepie.chains_daily_fundamentals import pull_growthepie_summary
 from .l2beat import pull_l2beat
 
@@ -101,3 +101,14 @@ def growthepie_chain_summary():
 
     GrowThePie.FUNDAMENTALS_SUMMARY.insert_to_clickhouse()
     GrowThePie.CHAIN_METADATA.insert_to_clickhouse()
+
+
+@app.command()
+def platforms_github_metrics():
+    """
+    Pull GitHub platform metrics (repo traffic) for the last 3 weeks,
+    and store them in daily-partitioned data on GCS.
+    """
+    from .github.platforms_github import pull_and_write_platforms_github_metrics
+
+    pull_and_write_platforms_github_metrics()
