47b2f816eaa7c84a7409d1918ecde1d43c42dadf
Pedro M Duarte <pedro@oplabs.co>
2025-01-10 12:14:03 -0500

    Update table ddl

--- ddl/bigquery/dailydata_defillama.dexs_by_chain_protocol_v1.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE `oplabs-tools-data.dailydata_defillama.dexs_by_chain_protocol_v1`
-WITH PARTITION COLUMNS (dt DATE) 
-OPTIONS (
-    format = 'PARQUET',
-    uris = ['gs://oplabs-tools-data-sink/defillama/dexs_by_chain_protocol_v1/*'],
-    hive_partition_uri_prefix = 'gs://oplabs-tools-data-sink/defillama/dexs_by_chain_protocol_v1',
-    require_hive_partition_filter = true,
-    decimal_target_types = ["NUMERIC", "BIGNUMERIC"]
-)
