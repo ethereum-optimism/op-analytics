f5bbc9d3f0a79635c3f2622bd25e348ccce775bd
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-01-16 11:18:58 -0500

    Add function to create bq external tables. Use a parameterized view in clickhouse so we can filter by partitions (#1262)

--- src/op_analytics/coreutils/bigquery/write.py
+++ src/op_analytics/coreutils/bigquery/write.py
@@ -9,40 +9,17 @@ import polars as pl
 from google.cloud import bigquery
 from google.api_core import exceptions
 
-from op_analytics.coreutils.env.aware import OPLabsEnvironment, current_environment
-from op_analytics.coreutils.gcpauth import get_credentials
 from op_analytics.coreutils.logger import human_rows, human_size, structlog
 from op_analytics.coreutils.time import date_fromstr, now
 
-log = structlog.get_logger()
+from .client import init_client
 
-_CLIENT: bigquery.Client | MagicMock | None = None
+log = structlog.get_logger()
 
 
 # This dataset is used to store staging tables used in upsert operations.
 # It is already configured with a default table expiration time of 1 day.
 UPSERTS_TEMP_DATASET = "temp_upserts"
-
-
-def init_client():
-    """BigQuery client with environment and testing awareness."""
-    global _CLIENT
-
-    if _CLIENT is None:
-        current_env = current_environment()
-
-        if current_env == OPLabsEnvironment.PROD:
-            _CLIENT = bigquery.Client(credentials=get_credentials())
-        else:
-            # MagicMock is used when running tests or when the PROD env is not specified.
-            # This is helpful to collect data and iterate on transformations without
-            # accidentally writing data to BigQuery before the final logic is in place.
-            _CLIENT = MagicMock()
-
-    if _CLIENT is None:
-        raise RuntimeError("BigQuery client was not properly initialized.")
-
-    return _CLIENT
 
 
 class OPLabsBigQueryError(Exception):
