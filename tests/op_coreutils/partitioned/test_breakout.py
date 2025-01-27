bd95a020dcb394721dcf66921f473872115ffc47
Pedro M Duarte <pedro@oplabs.co>
2025-01-27 12:48:01 -0500

    Fix bug in breakout partitiosn that was resulting in empty model output data

--- tests/op_coreutils/partitioned/test_breakout.py
+++ tests/op_coreutils/partitioned/test_breakout.py
@@ -88,55 +88,60 @@ def test_breakout_partitions():
     assert actual == EXPECTED_RESULT
 
 
+def pass_through(x):
+    return x
+
+
 def test_breakout_partitions_empty():
-    df = pl.DataFrame(
-        {
-            "dt": [
-                "2024-01-01",
-                "2024-01-01",
-                "2024-01-01",
-                "2024-01-01",
-                "2024-01-02",
-                "2024-01-02",
-                "2024-01-03",
-            ],
-            "chain": ["op", "op", "base", "base", "base", "base", "op"],
-            "c": ["some", "words", "here", "and", "some", "more", "blah"],
-        }
-    )
-
-    outputs: list[PartitionData] = []
-    for part in breakout_partitions(
-        df.filter(pl.col("dt") == ""),  # Filter out all data to get a default empty parquet file
-        partition_cols=["dt", "chain"],
-        default_partitions=[{"chain": "op", "dt": "2023-10-30"}],
-    ):
-        outputs.append(part)
-        assert part.df.columns == ["c"]
-
-    actual = []
-    for _ in outputs:
-        actual.append(
-            dict(
-                partitions=_.partition,
-                full_path=_.partition.path,
-                nu_rows=len(_.df),
-            )
+    for conversion in [pass_through, date_fromstr]:
+        df = pl.DataFrame(
+            {
+                "dt": [
+                    conversion("2024-01-01"),
+                    conversion("2024-01-01"),
+                    conversion("2024-01-01"),
+                    conversion("2024-01-01"),
+                    conversion("2024-01-02"),
+                    conversion("2024-01-02"),
+                    conversion("2024-01-03"),
+                ],
+                "chain": ["op", "op", "base", "base", "base", "base", "op"],
+                "c": ["some", "words", "here", "and", "some", "more", "blah"],
+            }
         )
 
-    actual.sort(key=lambda x: x["full_path"])
-    assert actual == [
-        {
-            "partitions": Partition(
-                [
-                    PartitionColumn(name="chain", value="op"),
-                    PartitionColumn(name="dt", value="2023-10-30"),
-                ]
-            ),
-            "full_path": "chain=op/dt=2023-10-30",
-            "nu_rows": 0,
-        }
-    ]
+        outputs: list[PartitionData] = []
+        for part in breakout_partitions(
+            df.filter(pl.lit(False)),  # Filter out all data to get a default empty parquet file
+            partition_cols=["dt", "chain"],
+            default_partitions=[{"chain": "op", "dt": "2023-10-30"}],
+        ):
+            outputs.append(part)
+            assert part.df.columns == ["c"]
+
+        actual = []
+        for _ in outputs:
+            actual.append(
+                dict(
+                    partitions=_.partition,
+                    full_path=_.partition.path,
+                    nu_rows=len(_.df),
+                )
+            )
+
+        actual.sort(key=lambda x: x["full_path"])
+        assert actual == [
+            {
+                "partitions": Partition(
+                    [
+                        PartitionColumn(name="chain", value="op"),
+                        PartitionColumn(name="dt", value="2023-10-30"),
+                    ]
+                ),
+                "full_path": "chain=op/dt=2023-10-30",
+                "nu_rows": 0,
+            }
+        ]
 
 
 def test_breakout_partitions_date_column_type():
@@ -219,61 +224,62 @@ def test_breakout_partitions_date_value_invalid():
 
 
 def test_breakout_partitions_partially_missing_default_partitions():
-    df = pl.DataFrame(
-        {
-            "dt": [
-                "2024-01-01",
-                "2024-01-01",
-                "2024-01-01",
-                "2024-01-01",
-            ],
-            "chain": ["op", "op", "op", "op"],
-            "c": ["some", "words", "here", "and"],
-        }
-    )
-
-    outputs: list[PartitionData] = []
-    for part in breakout_partitions(
-        df,
-        partition_cols=["dt", "chain"],
-        default_partitions=[
-            {"chain": "op", "dt": "2024-01-01"},
-            {"chain": "op", "dt": "2024-01-02"},
-        ],
-    ):
-        outputs.append(part)
-        assert part.df.columns == ["c"]
-
-    actual = []
-    for _ in outputs:
-        actual.append(
-            dict(
-                partitions=_.partition,
-                full_path=_.partition.path,
-                nu_rows=len(_.df),
-            )
+    for conversion in [pass_through, date_fromstr]:
+        df = pl.DataFrame(
+            {
+                "dt": [
+                    conversion("2024-01-01"),
+                    conversion("2024-01-01"),
+                    conversion("2024-01-01"),
+                    conversion("2024-01-01"),
+                ],
+                "chain": ["op", "op", "op", "op"],
+                "c": ["some", "words", "here", "and"],
+            }
         )
 
-    actual.sort(key=lambda x: x["full_path"])
-    assert actual == [
-        {
-            "partitions": Partition(
-                cols=[
-                    PartitionColumn(name="chain", value="op"),
-                    PartitionColumn(name="dt", value="2024-01-02"),
-                ]
-            ),
-            "full_path": "chain=op/dt=2024-01-02",
-            "nu_rows": 0,
-        },
-        {
-            "partitions": Partition(
-                cols=[
-                    PartitionColumn(name="dt", value="2024-01-01"),
-                    PartitionColumn(name="chain", value="op"),
-                ]
-            ),
-            "full_path": "dt=2024-01-01/chain=op",
-            "nu_rows": 4,
-        },
-    ]
+        outputs: list[PartitionData] = []
+        for part in breakout_partitions(
+            df,
+            partition_cols=["dt", "chain"],
+            default_partitions=[
+                {"chain": "op", "dt": "2024-01-01"},
+                {"chain": "op", "dt": "2024-01-02"},
+            ],
+        ):
+            outputs.append(part)
+            assert part.df.columns == ["c"]
+
+        actual = []
+        for _ in outputs:
+            actual.append(
+                dict(
+                    partitions=_.partition,
+                    full_path=_.partition.path,
+                    num_rows=len(_.df),
+                )
+            )
+
+        actual.sort(key=lambda x: x["full_path"])
+        assert actual == [
+            {
+                "partitions": Partition(
+                    cols=[
+                        PartitionColumn(name="dt", value="2024-01-01"),
+                        PartitionColumn(name="chain", value="op"),
+                    ]
+                ),
+                "full_path": "dt=2024-01-01/chain=op",
+                "num_rows": 4,
+            },
+            {
+                "partitions": Partition(
+                    cols=[
+                        PartitionColumn(name="dt", value="2024-01-02"),
+                        PartitionColumn(name="chain", value="op"),
+                    ]
+                ),
+                "full_path": "dt=2024-01-02/chain=op",
+                "num_rows": 0,
+            },
+        ]
