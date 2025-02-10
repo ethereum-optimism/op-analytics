668a59db67c1fde311da921c2cf152a6f5bb22f2
chuxinh <chuxin@oplabs.co>
2025-02-10 17:49:40 -0500

    add erc20 daily transfers

---
+++ src/op_analytics/transforms/erc20_transfers/create/export_fact_erc20_daily_transfers_v1.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS _placeholder_
+(
+    `chain` String,
+    `chain_id` Int32,
+    `dt` Date,
+    `network` String,
+    `contract_address` FixedString(66),
+    `num_transfers` UInt64,
+    `num_transactions` UInt64,
+    `num_blocks` UInt64,
+    `num_from_addresses` UInt64,
+    `num_to_addresses` UInt64,
+    `amount_raw` UInt256,
+    INDEX chain_idx chain TYPE minmax GRANULARITY 1,
+    INDEX contract_address_idx contract_address TYPE minmax GRANULARITY 1
+)
+ENGINE = ReplacingMergeTree()
+ORDER BY (chain, chain_id, dt, network, contract_address)
