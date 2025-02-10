9357b6841583246b7b7809da858db49bb0a62758
chuxinh <chuxin@oplabs.co>
2025-02-10 18:53:12 -0500

    rename

--- src/op_analytics/dagster/assets/transform.py
+++ src/op_analytics/dagster/assets/transform.py
@@ -12,3 +12,13 @@ def interop_transforms(context: AssetExecutionContext):
 
     result = execute_dt_transforms(group_name="interop", force_complete=True)
     context.log.info(result)
+
+
+@asset
+def transfer_transforms(context: AssetExecutionContext):
+    """Run transfer dataset transformations."""
+
+    from op_analytics.transforms.main import execute_dt_transforms
+
+    result = execute_dt_transforms(group_name="transfer", force_complete=True)
+    context.log.info(result)
