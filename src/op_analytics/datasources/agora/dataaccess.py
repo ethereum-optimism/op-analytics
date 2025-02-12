b7afd2ca3191fe64cfe861c21e8680dab485d112
Pedro M Duarte <pedro@oplabs.co>
2025-02-12 12:03:00 -0500

    Setup agora delegate changed events data pull

--- src/op_analytics/datasources/agora/dataaccess.py
+++ src/op_analytics/datasources/agora/dataaccess.py
@@ -1,39 +1,14 @@
-from enum import Enum
-import polars as pl
-
 from op_analytics.coreutils.logger import structlog
-from op_analytics.coreutils.partitioned.dailydata import write_daily_data
+from op_analytics.coreutils.partitioned.dailydata import DailyDataset
 
 log = structlog.get_logger()
 
 
-class Agora(str, Enum):
-    """Supported agora datasets."""
+class Agora(DailyDataset):
+    """Read data from agora.
 
-    DELEGATES = "delegates_v1"
-    DELEGATE_VOTES = "delegate_votes_v1"
-    PROPOSALS = "proposals_v1"
-    DELEGATORS = "delegators_v1"
-    DELEGATEES = "delegatees_v1"
+    For now we are only reading from the public GCS bucket.
+    Possibly more to come later on.
+    """
 
-    @property
-    def root_path(self) -> str:
-        return f"agora/{self.value}"
-
-    def write(
-        self,
-        dataframe: pl.DataFrame,
-        sort_by: list[str] | None = None,
-        force_complete: bool = False,
-    ):
-        """Write Agora dataset using daily partitioning."""
-        if dataframe.height == 0:
-            log.warning(f"Skipping write of empty dataframe for {self.value!r}")
-            return
-
-        log.info(f"Writing dataset {self.value!r} to {self.root_path!r}")
-        return write_daily_data(
-            root_path=self.root_path,
-            dataframe=dataframe,
-            sort_by=sort_by,
-        )
+    DELEGATE_CHANGED_EVENTS = "delegate_changed_events_v1"
