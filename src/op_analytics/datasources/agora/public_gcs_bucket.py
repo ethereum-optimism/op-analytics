53cc985d8f4cf6b07a2c12816f87c85a66701fc7
Thomas <166433899+thbialek@users.noreply.github.com>
2025-02-19 16:00:05 +0100

    Pull additional data from Agora gcs bucket (#1371)
    Co-authored-by: Pedro M Duarte <pmd323+lithium@gmail.com>

--- src/op_analytics/datasources/agora/public_gcs_bucket.py
+++ src/op_analytics/datasources/agora/public_gcs_bucket.py
@@ -24,15 +24,74 @@ DELEGATE_CHANGED_EVENTS_SCHEMA = pl.Schema(
     ]
 )
 
-TABLES = {
-    "delegate_changed_events": (Agora.DELEGATE_CHANGED_EVENTS, DELEGATE_CHANGED_EVENTS_SCHEMA)
+DELEGATES_SCHEMA = pl.Schema(
+    [
+        ("delegate", pl.String),
+        ("num_of_delegators", pl.Float64),
+        ("direct_vp", pl.Float64),
+        ("advanced_vp", pl.Float64),
+        ("voting_power", pl.Float64),
+        ("contract", pl.String),
+    ]
+)
+
+PROPOSALS_SCHEMA = pl.Schema(
+    [
+        ("proposal_id", pl.Float64),
+        ("contract", pl.String),
+        ("proposer", pl.String),
+        ("description", pl.String),
+        ("ordinal", pl.Int64),
+        ("created_block", pl.Int64),
+        ("start_block", pl.Int64),
+        ("end_block", pl.Int64),
+        ("queued_block", pl.Float64),
+        ("cancelled_block", pl.Float64),
+        ("executed_block", pl.Float64),
+        ("proposal_data", pl.String),
+        ("proposal_data_raw", pl.String),
+        ("proposal_type", pl.String),
+        ("proposal_type_data", pl.String),
+        ("proposal_results", pl.String),
+        ("created_transaction_hash", pl.String),
+        ("cancelled_transaction_hash", pl.String),
+        ("queued_transaction_hash", pl.String),
+        ("executed_transaction_hash", pl.String),
+    ]
+)
+
+VOTES_SCHEMA = pl.Schema(
+    [
+        ("transaction_hash", pl.String),
+        ("proposal_id", pl.Float64),
+        ("voter", pl.String),
+        ("support", pl.Int64),
+        ("weight", pl.Float64),
+        ("reason", pl.String),
+        ("block_number", pl.Int64),
+        ("params", pl.String),
+        ("start_block", pl.Int64),
+        ("description", pl.String),
+        ("proposal_data", pl.String),
+        ("proposal_type", pl.String),
+        ("contract", pl.String),
+        ("chain_id", pl.Int64),
+    ]
+)
+
+
+AGORA_PUBLIC_BUCKET_DATA = {
+    "delegate_changed_events": (Agora.DELEGATE_CHANGED_EVENTS, DELEGATE_CHANGED_EVENTS_SCHEMA),
+    "delegates": (Agora.DELEGATES, DELEGATES_SCHEMA),
+    "proposals_v2": (Agora.PROPOSALS, PROPOSALS_SCHEMA),
+    "votes": (Agora.VOTES, VOTES_SCHEMA),
 }
 
 
 def execute_pull():
     result = {}
 
-    for path, (table, schema) in TABLES.items():
+    for path, (table, schema) in AGORA_PUBLIC_BUCKET_DATA.items():
         full_path = f"{PREFIX}/{path}"
 
         with bound_contextvars(gcs_path=full_path):
