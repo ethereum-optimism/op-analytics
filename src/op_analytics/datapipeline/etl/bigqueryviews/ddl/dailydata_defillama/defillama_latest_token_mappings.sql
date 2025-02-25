f01ba16376947993ab265494cf36508fae479184
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2025-02-25 10:44:40 -0500

    update daily_dt

--- src/op_analytics/datapipeline/etl/bigqueryviews/ddl/dailydata_defillama/defillama_latest_token_mappings.sql
+++ src/op_analytics/datapipeline/etl/bigqueryviews/ddl/dailydata_defillama/defillama_latest_token_mappings.sql
@@ -5,5 +5,5 @@ SELECT
   MAX_BY(source_protocol, dt) AS source_protocol,
   MAX(dt) AS max_dt
  FROM `oplabs-tools-data.dailydata_defillama.dim_token_mappings_v1` 
-WHERE dt >= '1900-01-01'
+WHERE dt = '2000-01-01' --default dt per coreutils/partitioned/dailydata.py
 GROUP BY 1
