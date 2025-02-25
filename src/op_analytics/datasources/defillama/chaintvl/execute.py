a097910ba2aa05c895e15deb5fb2e27c9ae21e98
Pedro M Duarte <pedro@oplabs.co>
2025-02-24 22:14:24 -0500

    Save

--- src/op_analytics/datasources/defillama/chaintvl/execute.py
+++ src/op_analytics/datasources/defillama/chaintvl/execute.py
@@ -1,15 +1,16 @@
-from dataclasses import dataclass
-
 import polars as pl
 
 from op_analytics.coreutils.bigquery.write import most_recent_dates
 from op_analytics.coreutils.logger import structlog
+from op_analytics.coreutils.misc import raise_for_schema_mismatch
 from op_analytics.coreutils.partitioned.dailydatautils import dt_summary
-from op_analytics.coreutils.request import get_data, new_session
+from op_analytics.coreutils.request import new_session
 from op_analytics.coreutils.threads import run_concurrently
-from op_analytics.coreutils.time import dt_fromepoch, now_dt
+from op_analytics.coreutils.time import now_dt
 
 from ..dataaccess import DefiLlama
+from .chain import Chain
+from .metadata import ChainsMetadata
 
 log = structlog.get_logger()
 
@@ -21,30 +22,39 @@ CHAINS_TVL_ENDPOINT = "https://api.llama.fi/v2/historicalChainTvl/{slug}"
 TVL_TABLE_LAST_N_DAYS = 90
 
 
+CHAIN_DF_SCHEMA = {
+    "chain_name": pl.String,
+    "dt": pl.String,
+    "tvl": pl.Float64,
+}
+
+
 def execute_pull():
     session = new_session()
 
-    # Call the chains endpoint to find the list of chains tracked by DefiLLama.
-    chains_data = get_data(session, CHAINS_ENDPOINT)
-    dfl_chains_list = get_dfl_chains(chains_data)
-
-    # Call the config endpoint to find metadata for chains.
-    metadata = get_data(session, CHAINS_METADATA_ENDPOINT)
-    metadata_df = extract_chain_metadata(metadata["chainCoingeckoIds"], dfl_chains_list)
-
+    metadata = ChainsMetadata.fetch(session)
     DefiLlama.CHAINS_METADATA.write(
-        dataframe=metadata_df.with_columns(dt=pl.lit(now_dt())),
+        dataframe=metadata.df.with_columns(dt=pl.lit(now_dt())),
         sort_by=["chain_name"],
     )
 
     # Call the API endpoint for each stablecoin in parallel.
-    urls = construct_urls(dfl_chains_list, CHAINS_TVL_ENDPOINT)
-    historical_chain_tvl_data = run_concurrently(
-        lambda x: get_data(session, x), urls, max_workers=4
+    chains_data: dict[str, Chain] = run_concurrently(
+        function=lambda x: Chain.fetch(chain=x, session=session),
+        targets=metadata.chains,
+        max_workers=4,
     )
 
-    # Extract daily tvl from historical chains tvl data.
-    tvl_df = extract_chain_tvl_to_dataframe(historical_chain_tvl_data)
+    rows = []
+    for chain in chains_data.values():
+        rows.extend(chain.rows)
+    tvl_df = pl.DataFrame(rows, schema=CHAIN_DF_SCHEMA)
+
+    # Schema assertions to help our future selves reading this code.
+    raise_for_schema_mismatch(
+        actual_schema=tvl_df.schema,
+        expected_schema=pl.Schema(CHAIN_DF_SCHEMA),
+    )
 
     # Write balances.
     DefiLlama.HISTORICAL_CHAIN_TVL.write(
@@ -52,113 +62,7 @@ def execute_pull():
         sort_by=["chain_name"],
     )
 
-    result = DefillamaChains(
-        metadata_df=metadata_df,
-        tvl_df=tvl_df,
-    )
     return {
-        "metadata_df": dt_summary(result.metadata_df),
-        "tvl_df": dt_summary(result.tvl_df),
+        "metadata_df": dt_summary(metadata.df),
+        "tvl_df": dt_summary(tvl_df),
     }
-
-
-@dataclass
-class DefillamaChains:
-    """Metadata and tvl for all chains.
-
-    This is the result we obtain after fetching from the API and extracting the data
-    that we need to ingest.
-    """
-
-    metadata_df: pl.DataFrame
-    tvl_df: pl.DataFrame
-
-
-def extract_category_data(row: dict, category_type: str) -> int:
-    """Checks to see if chain metadata contains an EVM category"""
-    return 1 if "categories" in row and category_type in row["categories"] else 0
-
-
-def extract_layer(row: dict) -> str:
-    """Extracts L2 and L3 information from metadata. Defaults to L1."""
-    parent = row.get("parent")
-    parent_types = parent.get("types") if parent else []
-
-    if "L2" in parent_types:
-        return "L2"
-    elif "L3" in parent_types:
-        return "L3"
-    else:
-        return "L1"
-
-
-def extract_chain_metadata(chain_metadata: dict, dfl_chains: list) -> pl.DataFrame:
-    """
-    Extracts metadata from the config end point.
-    Data includes extra chains that Defillama does not track TVL for.
-    Entries may be null for certain chains.
-
-    Args:
-        chain_metadata: the "chainCoingeckoIds" response from config API
-        df: the summary df from the chains end point used to determine if
-            defillama tracks TVL for a particular chain
-
-    """
-    chain_metadata_records = []
-
-    for chain in chain_metadata:
-        chain_data = chain_metadata[chain]
-
-        chain_id = chain_data.get("chainId")
-        gecko_id = chain_data.get("geckoId")
-        cmc_id = chain_data.get("cmcId")
-        symbol = chain_data.get("symbol")
-        is_evm = extract_category_data(chain_data, "EVM")
-        is_superchain = extract_category_data(chain_data, "Superchain")
-        layer = extract_layer(chain_data)
-
-        chain_metadata_records.append(
-            {
-                "chain_name": chain,
-                "chain_id": chain_id,
-                "dfl_tracks_tvl": 1 if chain in dfl_chains else 0,
-                "is_evm": is_evm,
-                "is_superchain": is_superchain,
-                "layer": layer,
-                "is_rollup": 0 if layer == "L1" else 1,
-                "gecko_id": gecko_id,
-                "cmc_id": cmc_id,
-                "symbol": symbol,
-            }
-        )
-
-    return pl.DataFrame(chain_metadata_records)
-
-
-def get_dfl_chains(summary) -> list[str]:
-    """
-    Extracts chain names from defillama chains end point
-    """
-    return [chain.get("name") for chain in summary]
-
-
-def construct_urls(chain_list: list[str], endpoint: str) -> dict[str, str]:
-    """Build the collection of urls that we will fetch from DefiLlama."""
-
-    slugs = chain_list
-    urls = {slug: endpoint.format(slug=slug) for slug in slugs}
-
-    if not urls:
-        raise ValueError("No valid slugs provided.")
-    return urls
-
-
-def extract_chain_tvl_to_dataframe(data) -> pl.DataFrame:
-    """Extract historical TVL and transform into dataframe"""
-    records = [
-        {"chain_name": chain, "dt": dt_fromepoch(entry["date"]), "tvl": entry["tvl"]}
-        for chain, entries in data.items()
-        for entry in entries
-    ]
-
-    return pl.DataFrame(records)
