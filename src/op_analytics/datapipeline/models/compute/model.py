e532314b980dc3e212edc759bd2b90d2b471a218
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-02-27 09:04:36 -0500

    Fix bug in model registry - use function name as key (#1417)

--- src/op_analytics/datapipeline/models/compute/model.py
+++ src/op_analytics/datapipeline/models/compute/model.py
@@ -52,7 +52,7 @@ class ModelPath:
 @dataclass
 class PythonModel:
     # The registry stores all instances of PythonModel.
-    _registry: ClassVar[dict[ModelPath, "PythonModel"]] = {}
+    _registry: ClassVar[dict[str, "PythonModel"]] = {}
 
     # Thread-safe access to the registry.
     _registry_lock = Lock()
@@ -64,7 +64,7 @@ class PythonModel:
     model_func: ModelFunction
 
     def __post_init__(self):
-        self._registry[self.path] = self
+        self._registry[self.path.function_name] = self
 
     @property
     def name(self):
@@ -76,64 +76,45 @@ class PythonModel:
         return self.path.fq_model_path
 
     @classmethod
-    def get(cls, full_function_path: str) -> "PythonModel":
-        """Load a model based on its full function path.
+    def get(cls, function_name: str) -> "PythonModel":
+        """Load a model.
 
-        The python module where the module is declared is imported, which results
+        The python module where the model is defined is imported, which results
         in the model included in the PythonModel registry.
         """
 
-        # Do not support models defined on arbitary python modules.
-        if "." in full_function_path:
-            raise ValueError(f"error loading model: invalid path: {full_function_path}")
+        if function_name in cls._registry:
+            return cls._registry[function_name]
 
-        module = full_function_path
-        function_name = full_function_path
-
-        model_path = ModelPath(module, function_name)
+        if "." in function_name:
+            raise ValueError(f"error loading model: invalid path: {function_name}")
 
         with cls._registry_lock:
             current_models = set(cls._registry.keys())
 
-            if model_path not in cls._registry:
-                # Import the module so that the model gets registered.
-                # For now the module and model name are the same by convention, but
-                # that can change if we need to.
-                load_path = f"op_analytics.datapipeline.models.code.{module}"
-                importlib.import_module(load_path)
+            # Import the module so that the model gets registered.
+            # The module and function are the same by convention.
+            load_path = f"op_analytics.datapipeline.models.code.{function_name}"
+            importlib.import_module(load_path)
 
-                loaded_models = set(cls._registry.keys())
+            if load_path.endswith("code.account_abstraction"):
+                breakpoint()
 
-                num_loaded = len(loaded_models) - len(current_models)
-                if num_loaded == 0:
-                    raise Exception(
-                        f"error loading model: no modules were loaded from path: {load_path}"
-                    )
+            loaded_models = set(cls._registry.keys())
 
-                keys_loaded = loaded_models - current_models
-                if num_loaded > 1:
-                    raise Exception(
-                        f"error loading model: loaded more than one module from path: {load_path}: {keys_loaded}"
-                    )
-
-                key_loaded = list(keys_loaded)[0]
-
-                # Add a key alias to the registry. This is to support models defined in a directory
-                # using a model.py file: <MODEL_NAME>/model.py.
-                submodule_key = ModelPath(
-                    module=f"{function_name}.model",
-                    function_name=function_name,
+            num_loaded = len(loaded_models) - len(current_models)
+            if num_loaded == 0:
+                raise Exception(
+                    f"error loading model: no modules were loaded from path: {load_path}"
                 )
 
-                key_alias = ModelPath(
-                    module=function_name,
-                    function_name=function_name,
+            keys_loaded = loaded_models - current_models
+            if num_loaded > 1:
+                raise Exception(
+                    f"error loading model: loaded more than one module from path: {load_path}: {keys_loaded}"
                 )
 
-                if key_loaded == submodule_key:
-                    cls._registry[key_alias] = cls._registry[key_loaded]
-
-        return cls._registry[model_path]
+        return cls._registry[function_name]
 
     @property
     def func(self) -> ModelFunction:
