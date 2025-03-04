8c739f2c587cd810ab74cc8149390ad6878c293b
Pedro M Duarte <pedro@oplabs.co>
2025-03-04 11:03:57 -0500

    Save data eng office hours changes

--- src/op_analytics/transforms/governance/update/01_fact_delegate_votes_changed_v1.sql
+++ src/op_analytics/transforms/governance/update/01_fact_delegate_votes_changed_v1.sql
@@ -4,19 +4,15 @@ Decode DelegateVotesChanged event logs.
 
 */
 
-insert into _placeholder_
-
 select
-    t.block_timestamp,
+    t.dt,
+    t.timestamp AS block_timestamp,
     l.block_number,
     l.transaction_hash,
     l.log_index,
 
-    --Split into topics
-    assumeNotNull(splitByChar(',', ifNull(l.topics, ''))) as arr,
-
     --Decode the delegate address from topic1
-    concat('0x', lower(substr(arr[2], 27))) as delegate,
+    concat('0x', lower(substr(l.indexed_args[1], 27))) as delegate,
 
     --Extract previous_balance
     (
@@ -26,7 +22,7 @@ select
                     substring(substring(l.data, 3), 1, 64)
                 )
             )
-        ) / 1e18
+        ) 
     ) as previous_balance,
 
     --Extract new_balance
@@ -37,7 +33,7 @@ select
                     substring(substring(l.data, 3), 65, 64)
                 )
             )
-        ) / 1e18
+        )
     ) as new_balance,
 
     --Calculate the balance difference to get the (un)delegated OP amount
@@ -57,21 +53,21 @@ select
                     )
                 )
             )
-        ) / 1e18
+        ) 
     ) as delegation_amount
 
 from blockbatch_gcs.read_date(
     rootpath = 'ingestion/logs_v1'
     ,chain = 'op'
-    ,dt = '*-*-*'
+    ,dt = { dtparam: Date }
   ) l
 inner join blockbatch_gcs.read_date(
-    rootpath = 'ingestion/transactions_v1'
+    rootpath = 'ingestion/blocks_v1'
     ,chain = 'op'
-    ,dt = '*-*-*'
+    ,dt = { dtparam: Date }
   ) t
-on l.transaction_hash = t.hash
-and l.block_number = t.block_number
-where assumeNotNull(splitByChar(',', ifNull(l.topics, '')))[1] = '0xdec2bacdd2f05b59de34da9b523dff8be42e5e38e818c82fdb0bae774387a724' --Decode DelegateVotesChanged logs
-and lower(l.address) = '0x4200000000000000000000000000000000000042'
+on l.block_number = t.number
+
+where topic0 = '0xdec2bacdd2f05b59de34da9b523dff8be42e5e38e818c82fdb0bae774387a724' --Decode DelegateVotesChanged logs
+and l.address = '0x4200000000000000000000000000000000000042'
 settings use_hive_partitioning = 1;
