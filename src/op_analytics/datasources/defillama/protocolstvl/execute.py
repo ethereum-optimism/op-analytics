af0f030d7aacb6d2b3b087ce72c8ab06fb2dff9b
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-03-05 15:14:27 -0500

    More stable defillama protocols pull by using subprocesses (#1454)

--- src/op_analytics/datasources/defillama/protocolstvl/execute.py
+++ src/op_analytics/datasources/defillama/protocolstvl/execute.py
@@ -1,15 +1,14 @@
 import itertools
+import multiprocessing as mp
 from datetime import date
+from typing import Any, Callable, Iterable
 
-
-from op_analytics.coreutils.logger import structlog
-from op_analytics.coreutils.request import new_session
-from op_analytics.coreutils.threads import run_concurrently
+from op_analytics.coreutils.logger import memory_usage, structlog
 from op_analytics.coreutils.time import now_date
 
 from ..dataaccess import DefiLlama
 from .metadata import ProtocolMetadata
-from .utils import copy_to_gcs, fetch_and_write, get_buffered
+from .utils import Batch, copy_to_gcs, fetch_and_write, get_buffered
 
 log = structlog.get_logger()
 
@@ -20,11 +19,10 @@ TVL_TABLE_LAST_N_DAYS = 360
 def execute_pull(process_dt: date | None = None):
     """Daily pull protocol TVL data from DefiLlama."""
 
-    session = new_session()
     process_dt = process_dt or now_date()
 
     # Fetch the list of protocols and their metadata
-    metadata = ProtocolMetadata.fetch(session, process_dt)
+    metadata = ProtocolMetadata.fetch(process_dt)
     DefiLlama.PROTOCOLS_METADATA.write(
         dataframe=metadata.df,
         sort_by=["protocol_slug"],
@@ -34,13 +32,13 @@ def execute_pull(process_dt: date | None = None):
     slugs = metadata.slugs()
 
     # Fetch from API And write to buffer.
-    write_to_buffer(session, slugs, process_dt)
+    write_to_buffer(slugs, process_dt)
 
     # Copy data from buffer to GCS.
     return copy_to_gcs(process_dt=process_dt, last_n_days=TVL_TABLE_LAST_N_DAYS)
 
 
-def write_to_buffer(session, slugs: list[str], process_dt: date):
+def write_to_buffer(slugs: list[str], process_dt: date):
     """Pull data from DefiLlama and write to the ClickHouse buffer.
 
     This part was split up as a a separate function just so it is easy to add retries
@@ -53,9 +51,56 @@ def write_to_buffer(session, slugs: list[str], process_dt: date):
 
     # Fetch data and write to buffer for pending slugs.
     log.info(f"fetching and buffering data for {len(pending_ids)}/{len(slugs)} pending slugs")
-    run_concurrently(
-        function=lambda x: fetch_and_write(session, process_dt, x),
-        targets=list(itertools.batched(pending_ids, n=20)),
-        max_workers=4,
+
+    # Split into batches of "batch_size" slugs per batch.
+    batch_size = 120
+    num_pending = len(pending_ids)
+    total_tasks = (num_pending // batch_size) + (1 if num_pending % batch_size > 0 else 0)
+
+    # Loop over batches in subprocess to avoid memory usage creep. The subprocesses
+    # return memory back to the os.
+    run_in_subprocesses(
+        target=fetch_and_write,
+        tasks=batches(
+            process_dt=process_dt,
+            pending_ids=pending_ids,
+            n=batch_size,
+        ),
+        total_tasks=total_tasks,
     )
+
     log.info("done fetching and buffering data")
+
+
+def batches(process_dt: date, pending_ids: list[str], n: int):
+    """Split the pending ids into batches for processing."""
+
+    for batch in itertools.batched(pending_ids, n=n):
+        yield Batch(
+            process_dt=process_dt,
+            slugs=list(batch),
+        )
+
+
+def run_in_subprocesses[T](target: Callable[[T], Any], tasks: Iterable[T], total_tasks: int):
+    """Run the callable in a subprocess.
+
+    Using subprocesses helps control memory usage for long running jobs.
+    """
+    for i, task in enumerate(tasks):
+        log.info(f"running batch {i+1:03d}/{total_tasks:03d}", max_rss=memory_usage())
+        ctx = mp.get_context("spawn")
+        p = ctx.Process(target=target, args=(task,))
+        p.start()
+        try:
+            p.join()
+        except KeyboardInterrupt:
+            log.info("Keyboard interrupt received. Terminating spawned process")
+            p.terminate()
+            p.join()
+            raise
+
+        if p.exitcode != 0:
+            raise Exception(f"forked process failed with exitcode={p.exitcode}")
+
+        log.info("forked process success", max_rss=memory_usage())
