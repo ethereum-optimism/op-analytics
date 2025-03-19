f90da831c008bc27579bceacd45b9f89b3aad2dd
Pedro M Duarte <pedro@oplabs.co>
2025-03-19 09:40:38 -0400

    Save

--- src/op_analytics/transforms/governance/create/01_fact_delegate_votes_changed_v1.sql
+++ src/op_analytics/transforms/governance/create/01_fact_delegate_votes_changed_v1.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS _placeholder_
 (
     `dt` Date,
-    `block_timestamp`  DateTime,
-    `block_number`     UInt64,
-    `transaction_hash` FixedString(66),
-    `log_index`        UInt16,
-    `delegate`         String,
-    `previous_balance` UInt256,
-    `new_balance`      UInt256,
-    `delegation_amount`UInt256,
+    `block_timestamp`   DateTime,
+    `block_number`      UInt64,
+    `transaction_hash`  FixedString(66),
+    `log_index`         UInt16,
+    `delegate`          String,
+    `previous_balance`  UInt256,
+    `new_balance`       UInt256,
+    `delegation_amount` Int256,
     INDEX dt_idx dt TYPE minmax GRANULARITY 1,
 )
 ENGINE = ReplacingMergeTree
