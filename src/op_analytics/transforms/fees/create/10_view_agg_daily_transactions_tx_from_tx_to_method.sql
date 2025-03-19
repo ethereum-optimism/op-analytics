05001085488bb7beb59e3cc6b1c258e29629798a
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-03-19 18:08:33 -0400

    Add FINAL on transforms_fees views (#1497)

--- src/op_analytics/transforms/fees/create/10_view_agg_daily_transactions_tx_from_tx_to_method.sql
+++ src/op_analytics/transforms/fees/create/10_view_agg_daily_transactions_tx_from_tx_to_method.sql
@@ -1,9 +1,9 @@
-CREATE VIEW IF NOT EXISTS _placeholder_ AS
+CREATE OR REPLACE VIEW _placeholder_ AS
 
 SELECT 
   *
-FROM transforms_fees.agg_daily_transactions_grouping_sets
+FROM transforms_fees.agg_daily_transactions_grouping_sets FINAL
 WHERE 
-  from_address != '--' 
-  AND to_address != '--' 
+  from_address != '--'
+  AND to_address != '--'
   AND method_id != '--'
