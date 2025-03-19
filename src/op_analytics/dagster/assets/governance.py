ffe7bc941db21b0bd3e0b54f3aa24d8a20b230ec
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-03-19 17:34:53 -0400

    Schedule governance transforms on dagster (#1496)

--- src/op_analytics/dagster/assets/governance.py
+++ src/op_analytics/dagster/assets/governance.py
@@ -3,6 +3,8 @@ from dagster import (
     asset,
 )
 
+from op_analytics.transforms.main import execute_dt_transforms
+
 
 @asset
 def public_bucket(context: OpExecutionContext):
@@ -10,4 +12,18 @@ def public_bucket(context: OpExecutionContext):
     from op_analytics.datasources.governance import execute
 
     result = execute.execute_pull()
+    context.log.info(result)
+
+
+@asset
+def transforms_governance(context: OpExecutionContext):
+    """Execute governance transforms.
+
+    The governance transforms process the raw ingested data and enrich it using raw
+    onchain data. This includes:
+
+    - Decoding DelegateVotesChanged events.
+    - Joining with raw blocks to provide block timestamps.
+    """
+    result = execute_dt_transforms(group_name="governance", force_complete=True)
     context.log.info(result)
