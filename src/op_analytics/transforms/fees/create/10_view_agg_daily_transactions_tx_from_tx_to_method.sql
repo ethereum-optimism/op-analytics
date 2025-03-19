69d7325e2711aa42ff6cb70ec3a6134fc8616521
Pedro M Duarte <pedro@oplabs.co>
2025-03-19 17:55:12 -0400

    Save

--- src/op_analytics/transforms/fees/create/10_view_agg_daily_transactions_tx_from_tx_to_method.sql
+++ src/op_analytics/transforms/fees/create/10_view_agg_daily_transactions_tx_from_tx_to_method.sql
@@ -4,6 +4,6 @@ SELECT
   *
 FROM transforms_fees.agg_daily_transactions_grouping_sets FINAL
 WHERE 
-  from_address != '--' 
-  AND to_address != '--' 
+  from_address != '--'
+  AND to_address != '--'
   AND method_id != '--'
