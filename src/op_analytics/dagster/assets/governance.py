a5e25c8a371c4320eebe7c1ddf3800561a1aecb1
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-03-19 09:46:29 -0400

    Rename dagster asset agora -> governance (#1492)

--- src/op_analytics/dagster/assets/governance.py
+++ src/op_analytics/dagster/assets/governance.py
@@ -7,7 +7,7 @@ from dagster import (
 @asset
 def public_bucket(context: OpExecutionContext):
     """Pull Agora data."""
-    from op_analytics.datasources.agora import execute
+    from op_analytics.datasources.governance import execute
 
     result = execute.execute_pull()
     context.log.info(result)
