2be1ab0c9ab79eb846b325f1f68e9f3d139b5190
Pedro M Duarte <pedro@oplabs.co>
2025-03-31 14:53:17 -0400

    Update to use the overall historical endpoint instead of specific date

--- src/op_analytics/datasources/daopowerindex/historical.py
+++ src/op_analytics/datasources/daopowerindex/historical.py
@@ -6,7 +6,6 @@ import requests
 from op_analytics.coreutils.env.vault import env_get
 from op_analytics.coreutils.request import new_session, get_data
 from op_analytics.coreutils.misc import raise_for_schema_mismatch
-from op_analytics.coreutils.time import now_dt, datestr_subtract
 
 HISTORICAL_ENDPOINT = "https://api.daocpi.com/api/historic-cpi"
 
@@ -22,52 +21,33 @@ class HistoricalCPI:
     df: pl.DataFrame
 
     @classmethod
-    def fetch(
-        cls,
-        session: requests.Session | None = None,
-        fetch_dt: str | None = None,
-    ) -> "HistoricalCPI":
+    def fetch(cls, session: requests.Session | None = None) -> "HistoricalCPI":
         session = session or new_session()
 
         api_key = env_get("DAOCPI_API_KEY")
 
-        # If no fetch_dt is provided we will fetch the latest available data
-        # from the last 5 days.
-        if fetch_dt is None:
-            today = now_dt()
-            fetch_dt = [datestr_subtract(today, n) for n in range(5)]
-        else:
-            fetch_dt = [fetch_dt]
+        response = get_data(
+            session,
+            HISTORICAL_ENDPOINT,
+            headers={"x-api-key": api_key},
+        )
 
-        for dt in fetch_dt:
-            try:
-                response = get_data(
-                    session,
-                    HISTORICAL_ENDPOINT,
-                    headers={"x-api-key": api_key},
-                    params={"date": dt},
-                )
-            except requests.exceptions.HTTPError as ex:
-                if ex.response.status_code == 404:
-                    continue
-                else:
-                    raise ex
+        breakpoint()
 
-            df = pl.DataFrame(
-                [
-                    {
-                        "date": response["date"],
-                        "hhi": response["HHI"],
-                        "cpi": response["CPI"],
-                    }
-                ]
+        rows = []
+        for row in response:
+            rows.append(
+                {
+                    "date": row["date"],
+                    "hhi": row["HHI"],
+                    "cpi": row["CPI"],
+                }
             )
+        df = pl.DataFrame(rows)
 
-            raise_for_schema_mismatch(
-                actual_schema=df.schema,
-                expected_schema=pl.Schema(HISTORICAL_CPI_SCHEMA),
-            )
+        raise_for_schema_mismatch(
+            actual_schema=df.schema,
+            expected_schema=pl.Schema(HISTORICAL_CPI_SCHEMA),
+        )
 
-            return cls(df=df)
-
-        raise Exception(f"Failed to fetch data, no data found in: {fetch_dt}")
+        return cls(df=df)
