7154721a443f3066331af6728b4c8f4da065ad74
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2025-05-15 17:52:42 -0400

    add dagster assets (#1619)

--- src/op_analytics/dagster/assets/dune.py
+++ src/op_analytics/dagster/assets/dune.py
@@ -11,3 +11,20 @@ def dextrades(context: OpExecutionContext):
 
     result = execute_pull()
     context.log.info(result)
+
+@asset
+def eth_price_vol(context: OpExecutionContext):
+    """Pull ETH Volatility from Dune."""
+    from op_analytics.datasources.dune.eth_price_vol import execute_pull
+
+    result = execute_pull()
+    context.log.info(result)
+
+@asset
+def unichain_lm(context: OpExecutionContext):
+    """Pull unichain lm from Dune (Q225)."""
+    from op_analytics.datasources.dune.unichain_lm import execute_pull
+
+    result = execute_pull()
+    context.log.info(result)
+
