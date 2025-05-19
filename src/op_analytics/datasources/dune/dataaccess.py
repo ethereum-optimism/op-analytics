7a9a1694596cbfe0f86ac3f8008a631ba654905f
Pedro M Duarte <pmd323+lithium@gmail.com>
2025-05-19 08:16:18 -0400

    Bump dagster version to add dune (#1620)

--- src/op_analytics/datasources/dune/dataaccess.py
+++ src/op_analytics/datasources/dune/dataaccess.py
@@ -16,24 +16,5 @@ class Dune(DailyDataset):
     # Bespoke
     ETH_PRICE_VOL = "daily_eth_price_volatility_v1"
 
-
-from op_analytics.coreutils.logger import structlog
-from op_analytics.coreutils.partitioned.dailydata import DailyDataset
-
-log = structlog.get_logger()
-
-
-class Dune(DailyDataset):
-    """Dune Datasets.
-
-    This class includes utilities to read data from Dune queries
-    """
-
-    # DEX Trades
-    DEX_TRADES = "daily_dex_trades_summary_v1"
-
-    # Bespoke
-    ETH_PRICE_VOL = "daily_eth_price_volatility_v1"
-
     # Bespoke
     UNI_LM_2025 = "unichain_lm_summary_2025_v1"
