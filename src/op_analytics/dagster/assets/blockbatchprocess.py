1ac150cba7db557b61eabfc32938b3bdf25bf6b9
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2025-07-08 11:48:25 -0400

    range spec for update a and update b

--- src/op_analytics/dagster/assets/blockbatchprocess.py
+++ src/op_analytics/dagster/assets/blockbatchprocess.py
@@ -1,7 +1,9 @@
 from dagster import (
     OpExecutionContext,
     asset,
+    Config,
 )
+from typing import Optional
 
 from op_analytics.coreutils.partitioned.location import DataLocation
 from op_analytics.datapipeline.etl.blockbatch.main import compute_blockbatch
@@ -9,14 +11,18 @@ from op_analytics.datapipeline.orchestrate import normalize_blockbatch_models, n
 from op_analytics.dagster.utils.jobs import get_logs_url
 
 
+class UpdateAConfig(Config):
+    range_spec: Optional[str] = None  # Default is None
+
+
 @asset
-def update_a(context: OpExecutionContext):
+def update_a(context: OpExecutionContext, config: UpdateAConfig):
     context.log.info(f"LOGS URL: {get_logs_url()}")
 
     result = compute_blockbatch(
         chains=normalize_chains("ALL"),
         models=normalize_blockbatch_models("GROUPA"),
-        range_spec="m24hours",
+        range_spec=config.range_spec or "m24hours",
         read_from=DataLocation.GCS,
         write_to=DataLocation.GCS,
         dryrun=False,
@@ -26,14 +32,18 @@ def update_a(context: OpExecutionContext):
     context.log.info(result)
 
 
+class UpdateBConfig(Config):
+    range_spec: Optional[str] = None  # Default is None
+
+
 @asset
-def update_b(context: OpExecutionContext):
+def update_b(context: OpExecutionContext, config: UpdateBConfig):
     context.log.info(f"LOGS URL: {get_logs_url()}")
 
     result = compute_blockbatch(
         chains=normalize_chains("ALL"),
         models=normalize_blockbatch_models("GROUPB"),
-        range_spec="m24hours",
+        range_spec=config.range_spec or "m24hours",
         read_from=DataLocation.GCS,
         write_to=DataLocation.GCS,
         dryrun=False,
