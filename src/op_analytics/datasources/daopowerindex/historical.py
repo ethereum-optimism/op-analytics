b17b0a9076ccb589abbc4bdf3eaf7d185a56aad8
Dennis <dionisis@dioptx.com>
2025-07-29 14:38:26 +0300

    nit

--- src/op_analytics/datasources/daopowerindex/historical.py
+++ src/op_analytics/datasources/daopowerindex/historical.py
@@ -8,7 +8,6 @@ from op_analytics.coreutils.env.vault import env_get
 from op_analytics.coreutils.request import new_session, get_data
 from op_analytics.coreutils.misc import raise_for_schema_mismatch
 
-
 HISTORICAL_ENDPOINT = "https://api.daocpi.com/api/historic-cpi"
 
 HISTORICAL_CPI_SCHEMA = {
