03f3eb8de6278cf1feed741b710f234b046f403b
chuxinh <chuxin@oplabs.co>
2025-11-13 16:13:51 +0800

    update

---
+++ notebooks/misc_analysis/fee_market/config.py
@@ -0,0 +1,8 @@
+# ----------------------------------------
+# Configurations
+# ----------------------------------------
+import pandas as pd
+BLOCK_DATE= pd.Timestamp("2025-07-16")
+NUM_DAYS = 7
+
+CHAIN = 'op'
